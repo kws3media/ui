ce596ea0bbafda420fcbdf34451452744f72fb46
sharifclick <me.sharifahmed@gmail.com>
2018-09-06 20:39:15 +0600

    Kws3/helpers - Loader - story

---
+++ src/stories/loader.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from '@storybook/svelte';
+
+import "../scss/app.scss";
+
+import LoaderView from './views/LoaderView.html';
+
+
+
+
+
+storiesOf('Helpers | Loader ', module)
+  .add('Loader', () => ({
+    Component: LoaderView,
+  }))
