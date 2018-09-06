af44d3cd312eec4e1bcc11c42a7f38eea8939593
sharifclick <me.sharifahmed@gmail.com>
2018-09-06 18:51:08 +0600

    Kws3/helpers - Message - story

---
+++ src/stories/message.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from '@storybook/svelte';
+
+import "../scss/app.scss";
+
+import MessageView from './views/MessageView.html';
+
+
+
+
+
+storiesOf('Helpers | Message ', module)
+  .add('Message', () => ({
+    Component: MessageView,
+  }))
