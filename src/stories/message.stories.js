d072991dc34cbed3adcdde26523cfa2844ecec13
sharifclick <me.sharifahmed@gmail.com>
2018-09-13 16:51:40 +0600

    Update message story

--- src/stories/message.stories.js
+++ src/stories/message.stories.js
@@ -1,14 +1,30 @@
 import { storiesOf } from '@storybook/svelte';
+import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+
 
 import "../scss/app.scss";
 
 import MessageView from './views/MessageView.html';
 
 
-
-
-
 storiesOf('Helpers|Message', module)
-  .add('Message', () => ({
-    Component: MessageView,
-  }))
+  .addDecorator(withKnobs)
+  .add('Message', () => {
+
+    const color = select(
+      'color',
+      [
+        '', 'primary', 'info', 'warning',
+        'success', 'danger', 'dark', 'light'
+      ],
+      ''
+    );
+
+
+    return {
+      Component: MessageView,
+      data: {
+        color
+      }
+    }
+  })
