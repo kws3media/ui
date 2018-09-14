7a03bf26d9c0358ad0aece33ae7e60b88d054f7b
sharifclick <me.sharifahmed@gmail.com>
2018-09-14 20:40:01 +0600

    Confirm Button Basic story

---
+++ src/stories/confirm-button.stories.js
@@ -0,0 +1,42 @@
+import { storiesOf } from '@storybook/svelte';
+import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+
+import "../scss/app.scss";
+
+import ConfirmButtonView from './views/ConfirmButtonView.html';
+
+
+
+
+
+storiesOf('Buttons|ConfirmButton', module)
+  .addDecorator(withKnobs)
+  .add('ConfirmButton', () => {
+    const color = select(
+      'color',
+      [
+        'transparent', 'primary', 'info', 'warning',
+        'success', 'danger', 'dark', 'light'
+      ],
+      'danger'
+    );
+
+    const size = select(
+      'size',
+      [
+        '', 'medium', 'small', 'large'
+      ],
+      ''
+    );
+
+    const icon_only = boolean('icon_only', false);
+
+    return {
+      Component: ConfirmButtonView,
+      data: {
+        color,
+        size,
+        icon_only
+      },
+    }
+  });
