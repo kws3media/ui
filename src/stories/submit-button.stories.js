c88b5757921568c4a84441f2e7a64d880fce5b5f
ekhaled <me.khaled@gmail.com>
2018-09-17 15:51:45 +0100

    Submit button changes + docs changes

--- src/stories/submit-button.stories.js
+++ src/stories/submit-button.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/svelte';
-import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+import { withKnobs, select, boolean, text } from '@storybook/addon-knobs';
 
 import "../scss/app.scss";
 
@@ -13,12 +13,37 @@ storiesOf('Buttons|SubmitButton', module)
   .addDecorator(withKnobs)
   .add('SubmitButton', () => {
 
+    const color = select(
+      'color',
+      [
+        'transparent', 'primary', 'info', 'warning',
+        'success', 'danger', 'dark', 'light'
+      ],
+      'primary'
+    );
+
+    const size = select(
+      'size',
+      [
+        '', 'medium', 'small', 'large'
+      ],
+      ''
+    );
+
+    const defaultText = text('text', 'Save Changes');
+    const savedText = text('saved_text', 'Saved');
+    const errorText = text('error_text', 'Failed to Save');
 
     const disabled = boolean('disabled', false);
 
     return {
       Component: SubmitButtonView,
       data: {
+        size,
+        color,
+        text: defaultText,
+        saved_text: savedText,
+        error_text: errorText,
         disabled
       },
     }
