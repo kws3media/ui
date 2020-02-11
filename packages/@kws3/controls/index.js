a1385a0873c1252ba50c43d732c92f10632d4ed7
ekhaled <me.khaled@gmail.com>
2020-02-11 15:00:30 +0000

    custom checkbox component

--- packages/@kws3/controls/index.js
+++ packages/@kws3/controls/index.js
@@ -3,3 +3,5 @@ export { default as Toggle } from './components/Toggle/Toggle.html';
 export { default as ToggleControl } from './components/ToggleControl/ToggleControl.html';
 
 export { default as ToggleButtons } from './components/ToggleButtons/ToggleButtons.html';
+
+export { default as Checkbox } from './components/Checkbox/Checkbox.html';
