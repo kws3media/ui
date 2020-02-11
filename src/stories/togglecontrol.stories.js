e9e8f88cb86e894b3fec2dd03ff955e71cf26e35
ekhaled <me.khaled@gmail.com>
2020-02-11 10:56:03 +0000

    finalise toggleControl component

--- src/stories/togglecontrol.stories.js
+++ src/stories/togglecontrol.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/svelte';
-import { withKnobs, select, boolean , text} from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 
 import "../scss/app.scss";
 
@@ -12,40 +12,8 @@ import ToggleControlView from './views/ToggleControlView.html';
 storiesOf('Forms|Other Controls', module)
   .addDecorator(withKnobs)
   .add('ToggleControl', () => {
-    const color = select(
-      'color',
-      [
-        'primary', 'info',
-        'success', 'danger',
-        'dark', 'light'
-      ],
-      'success'
-    );
-
-    const size = select(
-      'size',
-      [
-        'small',
-        '',
-        'medium',
-        'large'
-      ],
-      ''
-    );
-
-    const on_text = text('on_text', '');
-    const off_text = text('off_text', '');
-
-    const disabled = boolean('disabled', false);
 
     return {
-      Component: ToggleControlView,
-      data: {
-        color,
-        size,
-        on_text,
-        off_text,
-        disabled
-      },
+      Component: ToggleControlView
     }
   });
