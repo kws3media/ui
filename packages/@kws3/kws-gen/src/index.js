5994625d75def79329d2211623cb8ef0d62d586e
Sharif Ahmed <me.sharifahmed@gmail.com>
2020-05-22 02:49:46 +0600

    views creation logic

--- packages/@kws3/kws-gen/src/index.js
+++ packages/@kws3/kws-gen/src/index.js
@@ -17,3 +17,32 @@ var section_name = sectionName(view_name);
 const CURR_DIR = process.cwd();
 var templates = menifest['templates'];
 var views_output = menifest['views_output'];
+
+
+async function main() {
+  templates.forEach(function (temp) {
+    var [name, _path] = Object.entries(temp)[0];
+
+    name = name.replace('_temp', section_name)
+    var temp_path = path.join(__dirname, _path);
+    var stats = fs.statSync(temp_path);
+
+    if (stats.isFile()) {
+      var contents = fs.readFileSync(temp_path, 'utf8');
+      contents = template.render(contents, { section_url, section_name })
+
+      var writePath = path.join(CURR_DIR, views_output + '/' + name + '.html');
+
+      console.log(writePath)
+      fs.writeFileSync(writePath, contents, 'utf8');
+    }
+  })
+
+}
+
+main().catch((err) => console.log(chalk.red.bold(err.message)));
+
+
+function sectionName(string) {
+  return string.charAt(0).toUpperCase() + string.toLowerCase().slice(1);
+}
