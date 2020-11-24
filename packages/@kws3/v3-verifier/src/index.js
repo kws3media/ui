b60c255689bb78b55d7926a3e711d8bd21c05cb5
ekhaled <me.khaled@gmail.com>
2020-11-24 16:25:44 +0000

    test package for version verification

---
+++ packages/@kws3/v3-verifier/src/index.js
@@ -0,0 +1,47 @@
+export function truncate(str, len) {
+  return (str && (str.length >= (len + 3))) ? str.substr(0, len) + '...' : str;
+}
+
+export function nl2br(input) {
+  if (!input) { return ''; }
+  return (input + '').replace(/\n/g, '<br>');
+}
+
+export function cloneObject(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+export var rAF = window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  function ( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+
+    window.setTimeout(callback, 1000 / 60);
+
+};
+
+export function debounce(fn, threshold, isAsap) {
+  var timeout, result;
+  function debounced() {
+    var args = arguments, context = this;
+    function delayed() {
+      if (!isAsap) {
+        result = fn.apply(context, args);
+      }
+      timeout = null;
+    }
+    if (timeout) {
+      clearTimeout(timeout);
+    } else if (isAsap) {
+      result = fn.apply(context, args);
+    }
+    timeout = setTimeout(delayed, threshold || 100);
+    return result;
+  }
+  debounced.cancel = function () {
+    clearTimeout(timeout);
+  };
+  return debounced;
+}
