66606c0bf280d86c5fc0239072342168011a875b
Sabir <sabirveli@gmail.com>
2021-08-13 20:22:35 +0530

    updated welcome story

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,14 +1,10 @@
 import WelcomeView from './views/Welcome.svelte';
 
-
-export default {
-  title: 'Welcome',
-  component: WelcomeView
-};
-
+export default {title: 'Welcome', component: WelcomeView };
 const Template = ({ onClick, ...args }) => ({
   Component: WelcomeView,
   props: args,
 });
 
 export const Welcome = Template.bind({});
+
