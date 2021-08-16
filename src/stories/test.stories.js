4065db208d1ecaf13ebacbe707fa6ba4ebe9b14d
Sabir <sabirveli@gmail.com>
2021-08-16 15:45:34 +0530

    remove old files

--- src/stories/test.stories.js
+++
@@ -1,9 +0,0 @@
-import TestView from './views/Test.svelte';
-
-export default {title: 'Test', component: TestView };
-const Template = ({ onClick, ...args }) => ({
-  Component: TestView,
-  props: args,
-});
-
-export const Test = Template.bind({});
