960dd2bb209265251c64889a986660cd3472a23c
Sabir <sabirveli@gmail.com>
2021-08-16 15:47:22 +0530

    welcome screen

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,6 +1,12 @@
 import WelcomeView from './views/Welcome.svelte';
 
-export default {title: 'Welcome', component: WelcomeView };
+export default {
+  title: 'Welcome',
+  component: WelcomeView,
+  previewTabs: {
+    docs: { hidden: true },
+  }
+};
 const Template = ({ onClick, ...args }) => ({
   Component: WelcomeView,
   props: args,
