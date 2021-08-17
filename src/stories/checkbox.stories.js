d967786658b19c2526baa95e6ce99a3028b19382
Sabir <sabirveli@gmail.com>
2021-08-17 14:45:58 +0530

    checkbox story

---
+++ src/stories/checkbox.stories.js
@@ -0,0 +1,25 @@
+import { action } from '@storybook/addon-actions';
+import CheckboxView from './views/Checkbox.svelte';
+import "../scss/app.scss";
+
+export default {
+  title: 'Controls/Checkbox',
+  component: CheckboxView,
+  argTypes:{
+    color: {
+      control: { type: "select", options: ["primary", "primary", "success", "danger", "warning", "dark", "light"] },
+    }
+  },
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+}
+
+const Template = (args) => ({ Component: CheckboxView, props: args });
+
+export const Checkbox = Template.bind({});
+Checkbox.args = {
+  color : "",
+  checked : false,
+  disabled : false
+};
+
+
