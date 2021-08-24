5e712a3ba56c67b05ab2ef4426194e1622a84c2a
ekhaled <me.khaled@gmail.com>
2021-08-24 15:21:24 +0100

    more test components

---
+++ bin/CompA.svelte
@@ -0,0 +1,18 @@
+<!--
+  @component
+
+
+  ### Events
+  - `mouseLeftProxied`
+
+-->
+<CompB
+  on:clicked
+  on:mouseLeft={({ detail }) => fire("mouseLeftProxied", detail)} />
+
+<script>
+  import CompB from "./CompB.svelte";
+  import { createEventDispatcher } from "svelte";
+
+  let fire = createEventDispatcher();
+</script>
---
+++ bin/CompB.svelte
@@ -0,0 +1,24 @@
+<!--
+  @component
+  
+
+  ### Events
+  - `clicked`
+  - `mouseLeft`
+
+-->
+<div on:click={click} on:mouseleave={msl} />
+
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  let fire = createEventDispatcher();
+
+  function click() {
+    fire("clicked");
+  }
+
+  function msl() {
+    fire("mouseLeft");
+  }
+</script>
