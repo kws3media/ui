4a3fc8e2de4ceb26424f9c3578ddc951c4e2eb6a
ekhaled <me.khaled@gmail.com>
2021-08-24 17:30:16 +0100

    bugfix

--- packages/@kws3/ui/index.js
+++ packages/@kws3/ui/index.js
@@ -1,10 +1,10 @@
-import Icon from './helpers/components/Icon.svelte';
-import {setDefaultIconType} from './helpers/components/Icon.svelte';
+import Icon from "./helpers/components/Icon.svelte";
+import { setDefaultIconType } from "./helpers/components/Icon.svelte";
 Icon.setDefaultIconType = setDefaultIconType;
-export {Icon};
+export { Icon };
 
-export { default as Message } from './helpers/components/Message.svelte';
-export { default as Checkbox } from './controls/components/Checkbox/Checkbox.svelte';
+export { default as Message } from "./helpers/components/Message.svelte";
+export { default as Checkbox } from "./controls/components/Checkbox/Checkbox.svelte";
 //export { default as ConfirmButton } from './buttons/components/ConfirmButton/ConfirmButton.svelte';
-export * as DeleteButton from './buttons/components/DeleteButton/DeleteButton.svelte';
+export { default as DeleteButton } from "./buttons/components/DeleteButton/DeleteButton.svelte";
 //export { default as SubmitButton } from './buttons/components/SubmitButton/SubmitButton.svelte';
