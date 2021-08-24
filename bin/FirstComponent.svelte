404dcfea138e06d273c270dc593cb4c9d7727771
ekhaled <me.khaled@gmail.com>
2021-08-24 15:49:13 +0100

    update test components

---
+++ bin/FirstComponent.svelte
@@ -0,0 +1,19 @@
+<!--
+  @component
+
+
+  @param [_] - No properties on this component
+
+-->
+<div>
+  <SecondComponent
+    on:clicked
+    on:mouseLeft={({ detail }) => fire("erase", detail)} />
+</div>
+
+<script>
+  import SecondComponent from "./SecondComponent.svelte";
+  import { createEventDispatcher } from "svelte";
+
+  const fire = createEventDispatcher();
+</script>
--- bin/SecondComponent.svelte
+++ bin/SecondComponent.svelte
@@ -2,17 +2,21 @@
   @component
   
 
+  @param [_] - No properties on this component
+
   ### Events
   - `clicked`
   - `mouseLeft`
 
 -->
-<div on:click={click} on:mouseleave={msl} />
+<div on:click={click} on:mouseleave={msl}>s</div>
 
 <script>
   import { createEventDispatcher } from "svelte";
 
   let fire = createEventDispatcher();
+
+  //export let x = 8;
 
   function click() {
     fire("clicked");
