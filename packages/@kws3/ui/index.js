d7ae168543e560d569e1f15884819e3c032e6caa
Sabir <sabirveli@gmail.com>
2021-08-24 14:08:00 +0530

    export with modules

--- packages/@kws3/ui/index.js
+++ packages/@kws3/ui/index.js
@@ -1,6 +1,10 @@
-export { default as Icon} from './helpers/components/Icon.svelte';
-export { default as Message} from './helpers/components/Message.svelte';
-export { default as Checkbox} from './controls/components/Checkbox/Checkbox.svelte';
+import Icon from './helpers/components/Icon.svelte';
+import {setDefaultIconType} from './helpers/components/Icon.svelte';
+Icon.setDefaultIconType = setDefaultIconType;
+export {Icon};
+
+export { default as Message } from './helpers/components/Message.svelte';
+export { default as Checkbox } from './controls/components/Checkbox/Checkbox.svelte';
 //export { default as ConfirmButton } from './buttons/components/ConfirmButton/ConfirmButton.svelte';
-export { default as DeleteButton } from './buttons/components/DeleteButton/DeleteButton.svelte';
+export * as DeleteButton from './buttons/components/DeleteButton/DeleteButton.svelte';
 //export { default as SubmitButton } from './buttons/components/SubmitButton/SubmitButton.svelte';
