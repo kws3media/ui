f1704ea442601606474092ec3a4b2751185a1328
Sabir <sabirveli@gmail.com>
2021-08-25 14:49:09 +0530

    decorator file for testing

---
+++ src/stories/CanvasDecorator.svelte
@@ -0,0 +1,15 @@
+<slot />
+
+<style global>
+  /*body {
+    color: #fff !important;
+  }*/
+</style>
+
+<script>
+  let dark = false;
+  var myParam = location.search.split("globals=")[1];
+  if (myParam.includes("333333")) {
+    dark = true;
+  }
+</script>
