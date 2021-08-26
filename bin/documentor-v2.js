fe4bc012570ed4dfa2f3b314c27117ed89fba043
Suneesh S K <suneeshsk3@gmail.com>
2021-08-26 20:48:04 +0530

    documentor v2

---
+++ bin/documentor-v2.js
@@ -0,0 +1,186 @@
+const fs = require("fs");
+const prettier = require("prettier");
+const sveltedocParser = require("sveltedoc-parser");
+
+const { readFileSync, writeFileSync } = fs;
+
+args = process.argv.slice(2);
+let file = args[0];
+
+try {
+  checkFileIsValid(file);
+} catch (e) {
+  console.log(e);
+  return;
+}
+
+function checkFileIsValid(file) {
+  let parts = file.split("."),
+    extension = parts.pop();
+  if (extension.toLowerCase() != "svelte") {
+    throw new Error(
+      "Only svelte files can be documented, this is a " + extension + " file"
+    );
+  }
+}
+
+let configFile = "./.prettierrc";
+
+let source = readFileSync(file, { encoding: "utf8", flag: "r" });
+
+global.COMPONENT_FEATURES = {};
+
+//pre-parse using sveltedoc-parser to pull out component features
+sveltedocParser
+  .parse({
+    version: 3,
+    fileContent: source,
+    encoding: "utf8",
+    features: ["name", "events", "slots", "data", "methods"],
+  })
+  .then((componentDoc) => {
+    global.COMPONENT_FEATURES = parseDoc(componentDoc);
+
+    prettier.clearConfigCache();
+
+    //format using custom plugin
+    prettier.resolveConfig(configFile).then((options) => {
+      let formatted = prettier.format(
+        source,
+        Object.assign(
+          {
+            parser: "svelte",
+            plugins: ["./bin/prettier-plugin-kws3"],
+            pluginSearchDirs: ["./bin/"],
+          },
+          options
+        )
+      );
+
+      writeFileSync(file, formatted);
+    });
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+function parseDoc(doc) {
+  let obj = {
+    name: "",
+    events: [],
+    slots: [],
+  };
+
+  obj.name = doc.name || "";
+  obj.events = fillEvents(doc);
+  obj.slots = fillSlots(doc);
+  obj.params = fillData(doc);
+
+  return obj;
+}
+
+function fillEvents(doc) {
+  let events = [];
+  if (doc.events.length) {
+    doc.events.forEach((e) => {
+      if (/*typeof e.parent == "undefined" && */ e.visibility == "public") {
+        let eventText = `- \`${e.name}\``;
+        let hasDescription = false;
+
+        //forwarded events pick up @component from the top when running on already documented file
+        //so we need to filter those out
+        if (e.description && !e.description.startsWith("@")) {
+          hasDescription = true;
+          eventText += ` - ${e.description}`;
+        }
+
+        if (e.keywords && e.keywords.length) {
+          e.keywords.forEach((k) => {
+            if (k.name == "returns" || k.name == "return") {
+              eventText += `${hasDescription ? ", " : ""} Returns: \`${
+                k.description
+              }\``;
+            }
+          });
+        }
+
+        events.push(eventText);
+      }
+    });
+  }
+
+  return events;
+}
+
+function fillSlots(doc) {
+  let slots = [];
+  if (doc.slots.length) {
+    doc.slots.forEach((s) => {
+      let slotText = `- \`<slot name="${s.name}"`;
+      let slotParams = "";
+      if (typeof s.params != "undefined" && s.params.length) {
+        let sp = [];
+        s.params.forEach((p) => {
+          sp.push(`{${p.name.replace(/[\{\}]/g, "")}}`);
+        });
+        slotParams = sp.join(" ");
+      }
+      slotText += " " + slotParams + " />`";
+      if (s.description) {
+        slotText += " - " + s.description;
+      }
+
+      slots.push(slotText);
+    });
+  }
+
+  return slots;
+}
+
+function fillData(doc) {
+  let params = [];
+
+  if (doc.data.length) {
+    doc.data.forEach((d) => {
+      let description = d.description || "";
+      let defaultValue = d.defaultValue || null;
+      let type = d.type.text;
+
+      if (d.kind == "const") {
+        description = "`CONST` " + description;
+      }
+
+      if (!defaultValue) {
+        if (type == "number") {
+          defaultValue += "";
+        } else if (type == "string") {
+          defaultValue = '""';
+        } else if (type == "array") {
+          defaultValue = "[]";
+        } else if (type == "object") {
+          defaultValue = "{}";
+        }
+      } else {
+        if (type == "string") {
+          defaultValue = '"' + defaultValue + '"';
+        }
+      }
+
+      params.push(
+        "@param {" +
+          d.type.text +
+          "} [" +
+          d.name +
+          "=" +
+          defaultValue +
+          "] - " +
+          description +
+          ", Default: `" +
+          defaultValue +
+          "`"
+      );
+    });
+  }
+
+  return params;
+}
