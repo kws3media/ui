343cc5cc33da6e8cfc1a944518d67737c9a868b0
Sabir <sabirveli@gmail.com>
2021-08-26 20:46:36 +0530

    Card Modal fixes

---
+++ src/stories/components/CardModal.svelte
@@ -0,0 +1,34 @@
+<CardModal
+  {title}
+  {size}
+  {is_active}
+  {closable}
+  {close_on_click_outside}
+  {has_footer}
+  {style}
+  {inner_style}
+  {classes}
+  {inner_class}
+  {cy}>
+  Your have 41 notifications
+</CardModal>
+
+<button
+  class="button is-primary"
+  type="button"
+  on:click={() => (is_active = true)}>Open Model</button>
+
+<script>
+  import { CardModal } from "@kws3/ui";
+  export let title = "Card Modal",
+    size = "",
+    is_active = false,
+    closable = true,
+    close_on_click_outside = false,
+    has_footer = true,
+    style = "",
+    inner_style = "",
+    classes = "",
+    inner_class = "",
+    cy = "";
+</script>
