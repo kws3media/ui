a5b5e064a87bbf21ccb365a7224894f6a458f870
Sabir <sabirveli@gmail.com>
2021-08-26 20:25:33 +0530

    working message box

---
+++ src/stories/components/Message.svelte
@@ -0,0 +1,28 @@
+<Message
+  {size}
+  {color}
+  {style}
+  {inner_style}
+  {classes}
+  {header_classes}
+  {inner_classes}
+  {title}
+  {has_title}
+  {dismissable}>
+  Your message here!!!
+</Message>
+
+<script>
+  import { Message } from "@kws3/ui";
+
+  export let size = "medium";
+  export let color = "info";
+  export let style = "",
+    inner_style = "",
+    classes = "",
+    header_classes = "",
+    inner_classes = "",
+    title = null,
+    has_title = false,
+    dismissable = false;
+</script>
