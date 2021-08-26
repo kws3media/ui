2fb811cbcb6453b1af49b0e14c91c0c7bf15e0c4
Sabir <sabirveli@gmail.com>
2021-08-26 19:53:10 +0530

    Notification component

---
+++ packages/@kws3/ui/helpers/components/Notification.svelte
@@ -0,0 +1,24 @@
+<div
+  class="notification is-{color} {light ? 'is-light' : ''}"
+  style="{style}"
+  bind:this="{_comp}">
+  {#if dismissable}
+    <button class="delete" on:click="{dismiss}"></button>
+  {/if}
+  <slot />
+</div>
+
+<script>
+  export let color = "info",
+    style = "",
+    light = false,
+    dismissable = false;
+
+  let _comp;
+
+  function dismiss() {
+    _comp.parentNode.removeChild(_comp);
+  }
+
+  // reviwed
+</script>
