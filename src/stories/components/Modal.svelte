ea9e96ebcc68eb0102d9adf5b8d1b5ace73169af
Sabir <sabirveli@gmail.com>
2021-08-26 20:56:10 +0530

    Modal stories

---
+++ src/stories/components/Modal.svelte
@@ -0,0 +1,31 @@
+<Modal
+  {size}
+  {is_active}
+  {closable}
+  {close_on_click_outside}
+  {style}
+  {inner_style}
+  {classes}
+  {inner_class}
+  {cy}>
+  <h1>This is a Heading</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+</Modal>
+
+<button
+  class="button is-primary"
+  type="button"
+  on:click={() => (is_active = true)}>Open Model</button>
+
+<script>
+  import { Modal } from "@kws3/ui";
+  export let size = "";
+  export let is_active = false;
+  export let closable = true;
+  export let close_on_click_outside = false;
+  export let style = "";
+  export let inner_style = "";
+  export let classes = "";
+  export let inner_class = "";
+  export let cy = "";
+</script>
