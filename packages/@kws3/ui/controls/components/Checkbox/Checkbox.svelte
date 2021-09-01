98b946c50456bfbed96b3d785056c8bf13581063
Sabir <sabirveli@gmail.com>
2021-09-01 16:07:54 +0530

    Checkbox story

--- packages/@kws3/ui/controls/components/Checkbox/Checkbox.svelte
+++ packages/@kws3/ui/controls/components/Checkbox/Checkbox.svelte
@@ -1,3 +1,26 @@
+<!--
+  @component
+
+
+  @param {string} [classes=""] - Classes property, Default: `""`
+  @param {string} [style=""] - Style property, Default: `""`
+  @param {string} [label_style=""] - Label_style property, Default: `""`
+  @param {string} [size=""] - Size property, Default: `""`
+  @param {string} [color=""] - Color property, Default: `""`
+  @param {boolean} [inverted=false] - Inverted property, Default: `false`
+  @param {boolean} [circle=false] - Circle property, Default: `false`
+  @param {boolean} [checked=false] - Checked property, Default: `false`
+  @param {boolean} [disabled=false] - Disabled property, Default: `false`
+  @param {string} [icon="check"] - Icon property, Default: `"check"`
+  @param {object} [icon_family=null] - Icon_family property, Default: `null`
+
+  ### Events
+  - `change`
+
+  ### Slots
+  - `<slot name="default"  />`
+
+-->
 <label
   class:has-icon={has_icon}
   class:is-inverted={inverted}
@@ -16,17 +39,80 @@
 <script>
   import { Icon } from "@kws3/ui";
 
-  export let classes = "",
-    style = "",
-    label_style = "",
-    size = "",
-    color = "",
-    inverted = false,
-    circle = false,
-    checked = false,
-    disabled = false,
-    icon = "check",
-    icon_family = null;
+  /**
+   * Size of the Button `small`, `medium`, `large`
+   * @type {'small'|'medium'|'large'}
+   */
+  export let size = "";
+
+  /**
+   * Color of the Button `primary`, `warning`, `info`, `danger`, `dark`, `light`
+   * @type {'primary'|'warning'|'info'|'danger'|'dark'|'light'}
+   */
+  export let color = "";
+
+  /**
+   * CSS classes
+   * @type {string}
+   * @defaultvalue empty
+   */
+  export let classes = "";
+
+  /**
+   * Inline styles
+   * @type {string}
+   * @defaultvalue empty
+   */
+  export let style = "";
+
+  /**
+   * Inverted Property
+   * @type {boolean}
+   * @defaultvalue false
+   */
+  export let inverted = false;
+
+  /**
+   * Circle checkbox
+   * @type {boolean}
+   * @defaultvalue false
+   */
+  export let circle = false;
+
+  /**
+   * Checkbox checked Property
+   * @type {boolean}
+   * @defaultvalue false
+   */
+  export let checked = false;
+
+  /**
+   * Boolean - true/false
+   * @type {boolean}
+   * @defaultvalue empty
+   */
+  export let disabled = false;
+
+  /**
+   * Icon of the checkbox
+   * @type {string}
+   * @defaultvalue check
+   */
+  export let icon = "check";
+
+  /**
+   * Inline style of Checkbox label
+   * @type {string}
+   * @defaultvalue check
+   */
+  export let label_style = "";
+
+  /**
+   * Family of Icon - fa, gg or unicons
+   * @type {string}
+   * @defaultvalue check
+   */
+  export let icon_family = null;
 
   $: has_icon = icon && icon != "";
 </script>
