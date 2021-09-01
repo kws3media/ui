854051f35158753d86c46c4b8141c0e73d4f6202
Sabir <sabirveli@gmail.com>
2021-09-01 19:33:00 +0530

    some design fixes

--- src/stories/controls/FileUpload/FileUpload.svelte
+++ src/stories/controls/FileUpload/FileUpload.svelte
@@ -1,5 +1,5 @@
 <div class="columns">
-  <div class="column">
+  <div class="column" style="width: 400px;">
     <KwsFileUpload
       on:file_chosen={(event) => onFileChosen(event)}
       on:file_uploaded
@@ -11,6 +11,22 @@
       {info}
       {max}
       {classes} />
+    <p class="is-block mt-2 has-text-success">This will succeed.</p>
+  </div>
+
+  <div class="column" style="width: 400px;">
+    <KwsFileUpload
+      on:file_chosen={(event) => onFileError(event)}
+      on:file_uploaded
+      on:file_upload_error
+      {allowed}
+      {disabled}
+      {message}
+      {key}
+      {info}
+      {max}
+      {classes} />
+    <p class="is-block mt-2 has-text-danger">This will fail.</p>
   </div>
 </div>
 
@@ -26,12 +42,23 @@
 
   function onFileChosen(event) {
     event.preventDefault();
-    let { getFile, progress, uploaded, error } = event.detail;
+    let { getFile, progress, uploaded } = event.detail;
     var file = getFile();
     var size = file.size;
     progress(0);
     setTimeout(() => {
       uploaded();
+    }, 1000);
+  }
+
+  function onFileError(event) {
+    event.preventDefault();
+    let { getFile, progress, error } = event.detail;
+    var file = getFile();
+    var size = file.size;
+    progress(0);
+    setTimeout(() => {
+      error("Error on upload");
     }, 1000);
   }
 </script>
