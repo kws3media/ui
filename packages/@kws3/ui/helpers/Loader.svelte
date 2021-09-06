92e8e0d3c52327acad7dfdc44e1db243cd2e8178
Suneesh S K <suneeshsk3@gmail.com>
2021-09-06 17:33:43 +0530

    is overlay prop

--- packages/@kws3/ui/helpers/Loader.svelte
+++ packages/@kws3/ui/helpers/Loader.svelte
@@ -1,21 +1,25 @@
 <!--
   @component
+  
 
-
-  @param {string} [spinner_color="light"] - Spinner color (light | warning | info | danger | primary | success), Default: `"light"`
+  @param {string} [spinner_color="grey"] - Spinner color (grey | light | warning | info | danger | primary | success), Default: `"grey"`
   @param {string} [spinner_size="medium"] - Spinner size (small | medium | large), Default: `"medium"`
   @param {string} [background_color="transparent"] - Background color (transparent | warning | info | danger | primary | success | link), Default: `"transparent"`
   @param {string} [background_size="medium"] - Background size (small | medium | large | halfheight | fullheight), Default: `"medium"`
   @param {boolean} [is_inline=false] - Loader is inline, Default: `false`
+  @param {boolean} [is_overlay=false] - Loader has overlay, Default: `false`
   @param {string} [style=""] - Inline CSS styles for loader, Default: `""`
 
 -->
-
-<div class="kws-loader hero is-{background_color} is-{background_size}" {style}>
+<div class={is_overlay ? "is-overlay" : ""}>
   <div
-    class="hero-body is-{spinner_color} is-{spinner_size}"
-    style={is_inline ? "padding:1rem" : ""}>
-    &nbsp;
+    class="kws-loader hero is-{background_color} is-{background_size}"
+    {style}>
+    <div
+      class="hero-body is-{spinner_color} is-{spinner_size}"
+      style={is_inline ? "padding:1rem" : ""}>
+      &nbsp;
+    </div>
   </div>
 </div>
 
@@ -41,6 +45,10 @@
      * Loader is inline
      */
     is_inline = false,
+    /**
+     * Loader has overlay
+     */
+    is_overlay = false,
     /**
      * Inline CSS styles for loader
      */
