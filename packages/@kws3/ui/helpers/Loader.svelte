191fdd588862ae1b1176f89ac0eb144e651db778
Suneesh S K <suneeshsk3@gmail.com>
2021-09-06 16:49:06 +0530

    prop changes

--- packages/@kws3/ui/helpers/Loader.svelte
+++ packages/@kws3/ui/helpers/Loader.svelte
@@ -1,35 +1,39 @@
 <!--
   @component
 
-  @param {string} [color="transparent"] - Color of the Loader (transparent | warning | info | danger | primary), Default: `"transparent"`
-  @param {string} [background_size="medium"] - Background size of the Loader (small | medium | large), Default: `"medium"`
-  @param {string} [spinner_size="medium"] - Spinner size of the Loader (small | medium | large), Default: `"medium"`
+
+  @param {string} [spinner_color="light"] - Spinner color (light | warning | info | danger | primary | success), Default: `"light"`
+  @param {string} [spinner_size="medium"] - Spinner size (small | medium | large), Default: `"medium"`
+  @param {string} [background_color="transparent"] - Background color (transparent | warning | info | danger | primary | success | link), Default: `"transparent"`
+  @param {string} [background_size="medium"] - Background size (small | medium | large | halfheight | fullheight), Default: `"medium"`
   @param {boolean} [is_inline=false] - Loader is inline, Default: `false`
   @param {string} [style=""] - Inline CSS styles for loader, Default: `""`
 
 -->
 
-<div
-  class="kws-loader hero is-{background_color} is-{background_size} has-spinner-{spinner_size}"
-  {style}>
-  <div class="hero-body" style={is_inline ? "padding:1rem" : ""}>&nbsp;</div>
+<div class="kws-loader hero is-{background_color} is-{background_size}" {style}>
+  <div
+    class="hero-body is-{spinner_color} is-{spinner_size}"
+    style={is_inline ? "padding:1rem" : ""}>
+    &nbsp;
+  </div>
 </div>
 
 <script>
   /**
-   * Spinner color (transparent | warning | info | danger | primary)
+   * Spinner color (grey | light | warning | info | danger | primary | success)
    */
-  export let spinner_color = "transparent",
+  export let spinner_color = "grey",
     /**
      * Spinner size (small | medium | large)
      */
     spinner_size = "medium",
     /**
-     * Background color (small | medium | large)
+     * Background color (transparent | warning | info | danger | primary | success | link)
      */
     background_color = "transparent",
     /**
-     * Background size (small | medium | large)
+     * Background size (small | medium | large | halfheight | fullheight)
      */
     background_size = "medium",
     /**
