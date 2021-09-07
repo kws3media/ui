da11f7a286ea315b27f7a82bb11a2e997f58e1ad
ekhaled <me.khaled@gmail.com>
2021-09-07 14:16:26 +0100

    update docs

--- packages/@kws3/ui/helpers/Dialog/Dialog.svelte
+++ packages/@kws3/ui/helpers/Dialog/Dialog.svelte
@@ -1,19 +1,28 @@
 <!--
   @component
-
+  
 
   @param {string} [title=""] - Title for dialog box, Default: `""`
-  @param {string} [text=""] - Text inside dialog box, Default: `""`
-  @param {'small'|'medium'|'large'} [size=""] - Size of the modal, Default: `""`
+  @param {'small'|'medium'|'large'} [size="small"] - Size of the dialog box, Default: `"small"`
+  @param {string} [input_value=""] - Default value inside text input displayed on `prompt()` dialogs, Default: `""`
   @param {string} [icon=""] - Icon on the dialog, Default: `""`
   @param {'primary'|'success'|'warning'|'info'|'danger'|'dark'|'light'} [icon_color="primary"] - Color of the Icon, Default: `"primary"`
-  @param {'small'|'medium'|'large'} [icon_size="null"] - Size of the Icon, Default: `"null"`
-  @param {string} [ok_button_text="Ok"] - Ok_button_text property, Default: `"Ok"`
-  @param {string} [ok_button_color="primary"] - Ok_button_color property, Default: `"primary"`
+  @param {'small'|'medium'|'large'} [icon_size=""] - Size of the Icon, Default: `""`
+  @param {string} [ok_button_text="Ok"] - Text to be displayed on the OK button, Default: `"Ok"`
+  @param {'primary'|'success'|'warning'|'info'|'danger'|'dark'|'light'} [ok_button_color="primary"] - Color of OK button, Default: `"primary"`
   @param {string} [ok_button_icon="check"] - Icon on the OK button, Default: `"check"`
-  @param {string} [cancel_button_text="Cancel"] - Cancel_button_text property, Default: `"Cancel"`
-  @param {string} [cancel_button_color=""] - Cancel_button_color property, Default: `""`
+  @param {string} [cancel_button_text="Cancel"] - Text to be displayed on the Cancel button, Default: `"Cancel"`
+  @param {''|'primary'|'success'|'warning'|'info'|'danger'|'dark'|'light'} [cancel_button_color=""] - Color of Cancel button, Default: `""`
   @param {string} [cancel_button_icon="ban"] - Icon on the Cancel button, Default: `"ban"`
+  @param {string} [_type=""] - Determines the type of dialog.
+
+For internal use only - not part of config object, Default: `""`
+  @param {string} [_text=""] - Text inside dialog box
+
+For internal use only - not part of config object, Default: `""`
+
+  ### Events
+  - `_done` - For internal use, not part of any configuration
 
 -->
 <svelte:window on:keydown={window_keydown} />
@@ -79,18 +88,17 @@
    */
   export let title = "",
     /**
-     * Size of the modal
+     * Size of the dialog box
      * @type {'small'|'medium'|'large'}
      */
     size = "small",
     /**
-     * Default value inside text input displayed on `prompt()`
+     * Default value inside text input displayed on `prompt()` dialogs
      * @type {string}
      */
     input_value = "",
     /**
      * Icon on the dialog
-     * @type {string}
      */
     icon = "",
     /**
@@ -102,15 +110,29 @@
      * Size of the Icon
      * @type {'small'|'medium'|'large'}
      */
-    icon_size = null,
+    icon_size = "",
+    /**
+     * Text to be displayed on the OK button
+     */
     ok_button_text = "Ok",
+    /**
+     * Color of OK button
+     * @type {'primary'|'success'|'warning'|'info'|'danger'|'dark'|'light'}
+     */
     ok_button_color = "primary",
     /**
      * Icon on the OK button
      * @type {string}
      */
     ok_button_icon = "check",
+    /**
+     * Text to be displayed on the Cancel button
+     */
     cancel_button_text = "Cancel",
+    /**
+     * Color of Cancel button
+     * @type {''|'primary'|'success'|'warning'|'info'|'danger'|'dark'|'light'}
+     */
     cancel_button_color = "",
     /**
      * Icon on the Cancel button
