171561b1aea2335abc88100901793e29145ea31e
Suneesh S K <suneeshsk3@gmail.com>
2021-09-07 20:47:03 +0530

    get a decorator

---
+++ src/stories/buttons/ProcessButton/ProcessButton.svelte
@@ -0,0 +1,33 @@
+<KwsButton
+  {text}
+  {color}
+  {icon}
+  {doing_icon}
+  {done_icon}
+  {doing_text}
+  {done_text}
+  on:doing={onDoing} />
+
+<script>
+  import { ProcessButton as KwsButton } from "@kws3/ui";
+
+  export let text = "Click Me",
+    icon = "check",
+    color = "primary",
+    doing_icon = "hourglass",
+    doing_text = "Doing...",
+    done_icon = "check",
+    done_text = "Done";
+
+  function onDoing(e, success = true) {
+    let { doing, done, error } = e.detail;
+    doing();
+    setTimeout(() => {
+      if (success) {
+        done();
+      } else {
+        error();
+      }
+    }, 2000);
+  }
+</script>
