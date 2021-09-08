33ea6a6d214bda7305c286d0acfbd91170de10a6
Sabir <sabirveli@gmail.com>
2021-09-08 19:24:35 +0530

    create story of maskedInput

---
+++ src/stories/forms/MaskedInput/MaskedInput.svelte
@@ -0,0 +1,43 @@
+<KwsMaskedInput
+  {mask}
+  {showMask}
+  {guide}
+  {guideOnOutput}
+  {keepCharPositions}
+  {inputElement}
+  {placeholder}
+  {classes}
+  bind:value />
+
+<script>
+  import { MaskedInput as KwsMaskedInput } from "@kws3/ui";
+  export let mask = [
+      /\d/,
+      /\d/,
+      /\d/,
+      /\d/,
+      " ",
+      /\d/,
+      /\d/,
+      /\d/,
+      /\d/,
+      " ",
+      /\d/,
+      /\d/,
+      /\d/,
+      /\d/,
+      " ",
+      /\d/,
+      /\d/,
+      /\d/,
+      /\d/,
+    ],
+    showMask = false,
+    guide = true,
+    guideOnOutput = false,
+    keepCharPositions = true,
+    placeholder = "",
+    classes = "",
+    inputElement = null,
+    value = null;
+</script>
