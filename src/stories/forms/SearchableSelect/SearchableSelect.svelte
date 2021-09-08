7f0158cad4f2370dd8f4c1deb7eecfbd209d9920
Sabir <sabirveli@gmail.com>
2021-09-08 17:18:03 +0530

    changable data

--- src/stories/forms/SearchableSelect/SearchableSelect.svelte
+++ src/stories/forms/SearchableSelect/SearchableSelect.svelte
@@ -16,28 +16,29 @@
 <script>
   import { SearchableSelect as KwsSearchableSelect } from "@kws3/ui";
 
-  export let brands = [
-      { id: 1, name: "LG" },
-      { id: 2, name: "Samsung" },
-      { id: 3, name: "Xiaomi" },
-      { id: 4, name: "Nokia" },
-      { id: 5, name: "Oppo" },
-      { id: 6, name: "Vivo" },
-      { id: 7, name: "Huawei" },
-      { id: 8, name: "Sony" },
-      { id: 9, name: "HTC" },
-      { id: 10, name: "Blackberry" },
-      { id: 11, name: "Apple" },
-    ],
-    selected_brand = "Blackberry";
-
   export let filter = "",
     style = "",
     dd_class = "",
     cy = "",
     open = false,
     disabled = false,
-    placeholder = "Choose a Brand...";
+    placeholder = "Choose a Brand...",
+    selected_brand = "Blackberry";
+
+  let brands = [
+    { id: 1, name: "LG" },
+    { id: 2, name: "Samsung" },
+    { id: 3, name: "Xiaomi" },
+    { id: 4, name: "Nokia" },
+    { id: 5, name: "Oppo" },
+    { id: 6, name: "Vivo" },
+    { id: 7, name: "Huawei" },
+    { id: 8, name: "Sony" },
+    { id: 9, name: "HTC" },
+    { id: 10, name: "Blackberry" },
+    { id: 11, name: "Apple" },
+  ];
+  export { brands as data };
 
   let klass = "";
   export { klass as class };
