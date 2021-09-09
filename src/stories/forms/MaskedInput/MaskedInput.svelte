1280d11221ba0ca5ae674f9a7aa1a572afbc64d6
Sabir <sabirveli@gmail.com>
2021-09-09 15:44:08 +0530

    set different input masks

--- src/stories/forms/MaskedInput/MaskedInput.svelte
+++ src/stories/forms/MaskedInput/MaskedInput.svelte
@@ -1,38 +1,91 @@
-<KwsMaskedInput
-  {mask}
-  {showMask}
-  {guide}
-  {guideOnOutput}
-  {keepCharPositions}
-  {inputElement}
-  {placeholder}
-  class={klass}
-  bind:value />
+<div class="columns">
+  <div class="column is-4">
+    <label for="us-phone">US-Phone <small>(masked input)</small></label>
+    <KwsMaskedInput
+      mask={us_phpne_mask}
+      {showMask}
+      {guide}
+      {guideOnOutput}
+      {keepCharPositions}
+      {inputElement}
+      {placeholder}
+      class={klass}
+      bind:value={value1} />
+  </div>
+  <div class="column is-4">
+    <label for="credit-card">Credit Card <small>(masked input)</small></label>
+    <KwsMaskedInput
+      mask={credit_card_mask}
+      {showMask}
+      {guide}
+      {guideOnOutput}
+      {keepCharPositions}
+      {inputElement}
+      {placeholder}
+      class={klass}
+      bind:value={value2} />
+  </div>
+  <div class="column is-4">
+    <label for="date">Date <small>(masked input)</small></label>
+    <KwsMaskedInput
+      mask={date_mask}
+      {showMask}
+      {guide}
+      {guideOnOutput}
+      {keepCharPositions}
+      {inputElement}
+      {placeholder}
+      class={klass}
+      bind:value={value3} />
+  </div>
+</div>
 
 <script>
   import { MaskedInput as KwsMaskedInput } from "@kws3/ui";
-  export let mask = [
-      /\d/,
-      /\d/,
-      /\d/,
-      /\d/,
-      " ",
-      /\d/,
-      /\d/,
-      /\d/,
-      /\d/,
-      " ",
-      /\d/,
-      /\d/,
-      /\d/,
-      /\d/,
-      " ",
-      /\d/,
-      /\d/,
-      /\d/,
-      /\d/,
-    ],
-    showMask = false,
+  let us_phpne_mask = [
+    "(",
+    /[1-9]/,
+    /\d/,
+    /\d/,
+    ")",
+    " ",
+    /\d/,
+    /\d/,
+    /\d/,
+    "-",
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+  ];
+  let credit_card_mask = [
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    " ",
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    " ",
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    " ",
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+  ];
+  let date_mask = [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/];
+
+  let value1 = "",
+    value2 = "",
+    value3 = "";
+
+  export let showMask = false,
     guide = true,
     guideOnOutput = false,
     keepCharPositions = true,
