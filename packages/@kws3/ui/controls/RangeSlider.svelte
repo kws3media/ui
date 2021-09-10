de6c02a6cd78a96b823462d8fde3876f7b3be38c
Sabir <sabirveli@gmail.com>
2021-09-10 15:46:49 +0530

    Doc fixes

--- packages/@kws3/ui/controls/RangeSlider.svelte
+++ packages/@kws3/ui/controls/RangeSlider.svelte
@@ -1,35 +1,42 @@
+<!--
+  @component
+
+
+  @param {number} [min=0] - Minimum Range, Default: `0`
+  @param {number} [max=100] - Maximum Range, Default: `100`
+  @param {number} [value=50] - Starting Value, Default: `50`
+  @param {string} [class=""] - Additional classes, Default: `""`
+
+  ### Events
+  - `change` - Change event
+
+-->
 <div class="range-control">
-  <input
-    class="input {classes}"
-    type="range"
-    bind:value
-    on:change
-    {min}
-    {max} />
+  <!--Change event-->
+  <input class="input {klass}" type="range" bind:value on:change {min} {max} />
   <output style={computedStyle}>{value}</output>
 </div>
 
 <script>
   /**
    * Minimum Range
-   * @type {number}
    */
   export let min = 0,
     /**
      * Maximum Range
-     * @type {number}
      */
     max = 100,
     /**
      * Starting Value
-     * @type {number}
      */
-    value = 50,
-    /**
-     * Additional classes
-     * @type {string}
-     */
-    classes = "";
+    value = 50;
+
+  /**
+   * Additional classes
+   * @type {string}
+   */
+  let klass = "";
+  export { klass as class };
 
   let computedStyle = "";
 
