1a8b05b6ff5a28beec541b7b239bcb390165fed8
ekhaled <me.khaled@gmail.com>
2021-09-14 13:05:47 +0100

    misc and doc fixes

--- packages/@kws3/ui/controls/RangeSlider.svelte
+++ packages/@kws3/ui/controls/RangeSlider.svelte
@@ -2,26 +2,28 @@
   @component
 
 
-  @param {number} [min=0] - Lowest value of the selectable range, Default: `0`
-  @param {number} [max=100] - Highest value of the selectable range, Default: `100`
-  @param {number} [value=0] - Default set value before any user interaction, Default: `0`
-  @param {boolean} [output=false] - Display the current position of the Slider as a value, Default: `false`
+  @param {number} [min=0] - Minumum permitted value, Default: `0`
+  @param {number} [max=100] - Maximum permitted value, Default: `100`
+  @param {number} [value=0] - Current value
+
+This property can be bound to, to fetch the current value, Default: `0`
+  @param {boolean} [output=false] - Displays the current value, Default: `false`
   @param {boolean} [disabled=false] - Disables the Slider, Default: `false`
   @param {''|'small'|'medium'|'large'} [size=""] - Size of the Slider, Default: `""`
   @param {''|'primary'|'warning'|'success'|'info'|'danger'|'dark'|'light'} [color=""] - Colour of the Slider, Default: `""`
   @param {string} [style=""] - Inline CSS for the Slider, Default: `""`
-  @param {boolean} [circle=false] - Uses a round slider instead of the default square, Default: `false`
-  @param {boolean} [tooltip=false] - Displays the current position value of the slider as a tooltip
+  @param {boolean} [circle=false] - Uses a round slider thumb instead of the default square, Default: `false`
+  @param {boolean} [tooltip=false] - Displays the current value of the Slider as a tooltip
 
 `output` has to be enabled for this to work, Default: `false`
   @param {string} [class=""] - CSS classes for the slider, Default: `""`
 
   ### Events
-  - `change` - Triggered whenever any change is made to the slider value
+  - `change` - Native input change event
 
 -->
 <div class="range-control">
-  <!--Triggered whenever any change is made to the slider value-->
+  <!--Native input change event-->
   <input
     class="slider is-fullwidth
     {outputClass}
@@ -39,31 +41,23 @@
   {/if}
 </div>
 
-<style>
-  .range-control {
-    position: relative;
-  }
-  input[type="range"].slider:not([orient="vertical"]).has-output-tooltip
-    + output {
-    top: 2rem !important;
-  }
-</style>
-
 <script>
   /**
-   * Lowest value of the selectable range
+   * Minumum permitted value
    */
   export let min = 0,
     /**
-     * Highest value of the selectable range
+     * Maximum permitted value
      */
     max = 100,
     /**
-     * Default set value before any user interaction
+     * Current value
+     *
+     * This property can be bound to, to fetch the current value
      */
     value = 0,
     /**
-     * Display the current position of the Slider as a value
+     * Displays the current value
      */
     output = false,
     /**
@@ -89,7 +83,7 @@
      */
     circle = false,
     /**
-     * Displays the current position value of the slider as a tooltip
+     * Displays the current value of the Slider as a tooltip
      *
      * `output` has to be enabled for this to work
      */
