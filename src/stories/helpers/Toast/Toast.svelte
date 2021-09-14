fd840f81d840e62da1c7ba08f7383402e3a90d97
Suneesh S K <suneeshsk3@gmail.com>
2021-09-14 20:04:34 +0530

    toast story

---
+++ src/stories/helpers/Toast/Toast.svelte
@@ -0,0 +1,14 @@
+<div class="columns">
+  <div class="column has-text-centered">
+    <button class="button is-primary" type="button" on:click={create}
+      >Open Toast</button>
+  </div>
+</div>
+
+<script>
+  import { Toast as KwsToast } from "@kws3/ui";
+
+  function create() {
+    KwsToast.create({ message: "I am a Toast..." });
+  }
+</script>
