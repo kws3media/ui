5dcdfdad3120cdbd03c7bc1041eca7c45c2b3abd
Suneesh S K <suneeshsk3@gmail.com>
2021-09-15 13:27:12 +0530

    toast position

--- packages/@kws3/ui/helpers/Toast/Toast.svelte
+++ packages/@kws3/ui/helpers/Toast/Toast.svelte
@@ -1,4 +1,4 @@
-<div class="toast">
+<div class="toast is-{position}">
   <div class="notification is-{color}">
     {#if is_dismissable}
       <button class="delete" on:click={destroy} />
@@ -29,6 +29,11 @@
      * Message background color
      */
     color = "info",
+    /**
+     * Position of the message
+     * @type {'top' | 'bottom' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'}
+     */
+    position = "bottom-right",
     /**
      * Dismissable message
      */
