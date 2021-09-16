0dfaf9c62ec127af8776e5e9715a827dd4ec5a14
Suneesh S K <suneeshsk3@gmail.com>
2021-09-16 12:10:45 +0530

    story fixes

--- src/stories/helpers/Toast/Toast.svelte
+++ src/stories/helpers/Toast/Toast.svelte
@@ -1,23 +1,21 @@
 <div class="columns">
   <div class="column has-text-centered">
-    <button
-      class="button is-primary"
-      type="button"
-      on:click={() => create({ position: "top-left" })}>Toast Top Left</button>
-  </div>
-  <div class="column has-text-centered">
-    <button
-      class="button is-info"
-      type="button"
-      on:click={() => create({ position: "bottom-right" })}
-      >Toast Bottom left</button>
+    <button class="button is-primary" type="button" on:click={create}
+      >Create Toast</button>
   </div>
 </div>
 
 <script>
   import { Toast as KwsToast } from "@kws3/ui";
 
-  function create(prop) {
-    KwsToast.push({ message: "I am a Toast...", ...prop });
+  export let message = "I am a Toast...",
+    duration = 3000,
+    color = "primary",
+    position = "bottom-right",
+    is_dismissable = true,
+    is_persistent = false;
+
+  function create() {
+    KwsToast.push({ ...$$props });
   }
 </script>
