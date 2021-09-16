2ff69c3aeac89e24a43944dd4a34befd61a02e97
Suneesh S K <suneeshsk3@gmail.com>
2021-09-16 21:51:23 +0530

    toast set default placement

--- packages/@kws3/ui/helpers/Toast/Toast.svelte
+++ packages/@kws3/ui/helpers/Toast/Toast.svelte
@@ -6,18 +6,21 @@
   @param {string} [message=""] - Message for display, Default: `""`
   @param {number} [duration=3000] - Duration for display message, Default: `3000`
   @param {'warning'|'info'|'danger'|'primary'|'success'} [color="primary"] - Message background color, Default: `"primary"`
-  @param {'top'|'bottom'|'top-left'|'top-right'|'bottom-left'|'bottom-right'} [position="bottom-right"] - Position of the message, Default: `"bottom-right"`
+  @param {'top'|'bottom'|'top-left'|'top-right'|'bottom-left'|'bottom-right'} [position=""] - Position of the message, Default: `""`
   @param {boolean} [is_dismissable=true] - Dismissable message, Default: `true`
   @param {boolean} [is_persistent=false] - Persistent message, Default: `false`
   @param {string} [context=""] - Context value, Default: `""`
   @param {object} [component=null] - Custom component, Default: `null`
+
+  ### Module
+  @param {function} [setDefaultPlacement(position)] - It can set default position
 
   ### Events
   - `destroy`
 
 -->
 
-<div class="toast is-{position}">
+<div class="toast is-{usedPosition}">
   <div class="notification is-{color}">
     {#if component}
       <svelte:component this={component} {...$$props} />
@@ -41,6 +44,21 @@
     {/if}
   </div>
 </div>
+
+<script context="module">
+  let globalPosition;
+
+  /**
+   * It can set default position
+   *
+   * @description
+   * import { Toast } from `@kws3/ui`;
+   * Toast.setDefaultPlacement("bottom-left");
+   */
+  export function setDefaultPlacement(position) {
+    globalPosition = position;
+  }
+</script>
 
 <script>
   import { onMount, onDestroy, createEventDispatcher } from "svelte";
@@ -68,7 +86,7 @@
      * Position of the message
      * @type {'top'|'bottom'|'top-left'|'top-right'|'bottom-left'|'bottom-right'}
      */
-    position = "bottom-right",
+    position = "",
     /**
      * Dismissable message
      */
@@ -86,7 +104,16 @@
      */
     component = null;
 
-  let timeout;
+  let timeout, usedPosition;
+
+  $: {
+    usedPosition =
+      position && position != ""
+        ? position
+        : globalPosition
+        ? globalPosition
+        : "bottom-right";
+  }
 
   const destroy = () => fire("destroy", $$props);
 
--- packages/@kws3/ui/helpers/Toast/index.js
+++ packages/@kws3/ui/helpers/Toast/index.js
@@ -1,4 +1,4 @@
-import Toast from "./Toast.svelte";
+import Toast, { setDefaultPlacement } from "./Toast.svelte";
 import ToastOutput, { notifications } from "./ToastOutput.svelte";
 
 const pushToast = (props) => notifications.push(props);
@@ -6,6 +6,7 @@ const closeToast = (props) => notifications.pop(props);
 
 Toast.push = pushToast;
 Toast.close = closeToast;
+Toast.setDefaultPlacement = setDefaultPlacement;
 
 export { ToastOutput, pushToast, closeToast };
 export default Toast;
