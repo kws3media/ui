0d14cf5a6f505c0a44eb75badcec36e7dc601482
Sabir <sabirveli@gmail.com>
2021-09-22 17:41:41 +0530

    package update

--- packages/@kws3/ui/controls/Steps.svelte
+++ packages/@kws3/ui/controls/Steps.svelte
@@ -1,36 +1,64 @@
-<div class="steps {klass} is-{size}">
+<ul class="steps {klass} is-{size}">
   {#each steps as step, idx}
-    <div
+    <li
       class="step-item is-{color} {active_index >= idx ? 'is-active' : ''}"
       on:click={() => navigateStep(idx)}
-      style="cursor:pointer;">
-      <div class="step-marker">{idx + 1}</div>
-      {#if steps[idx].name || steps[idx].description}
+      style={clickable ? "cursor:pointer;" : ""}>
+      <div class="step-marker">
+        {#if icon_only}
+          <Icon icon={step_icon} />
+        {:else}
+          {idx + 1}
+        {/if}
+      </div>
+      {#if step.name || step.description}
         <div class="step-details">
-          <p class="step-title">{steps[idx].name}</p>
-          {#if steps[idx].description}
-            <p>{steps[idx].description}</p>
+          <p class="step-title">{step.name}</p>
+          {#if step.description}
+            <p>{step.description}</p>
           {/if}
         </div>
       {/if}
-    </div>
+    </li>
   {/each}
-</div>
+</ul>
 
 <script>
-  import { onMount } from "svelte";
-
+  import { Icon } from "@kws3/ui";
   /**
-   * Minumum permitted value
+   * Object, which contains name and description of each steps
+   * @any
    */
   export let steps = [],
+    /**
+     * Index used to determine which step needs to be `active`
+     */
     active_index = 0,
+    /**
+     * Size of the step
+     * @type {''|'small'|'medium'|'large'}
+     */
     size = "small",
+    /**
+     * Color of the step
+     * @type {''|'primary'|'warning'|'success'|'info'|'danger'|'dark'|'light'}
+     */
     color = "primary",
-    clickable = false;
+    /**
+     * Determine step is clickable
+     */
+    clickable = false,
+    /**
+     * Show step icon given in `step_icon` property instead of numbers
+     */
+    icon_only = false,
+    /**
+     * Icon used to displayed in steps
+     */
+    step_icon = "check";
 
   /**
-   * CSS classes for the slider
+   * CSS classes for the steps container
    */
   let klass = "";
   export { klass as class };
