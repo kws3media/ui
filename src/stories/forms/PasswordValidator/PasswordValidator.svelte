c2bbce47b9459a638e22a75ad445412f84809473
Sabir <sabirveli@gmail.com>
2021-09-22 19:40:40 +0530

    pwd validator story

---
+++ src/stories/forms/PasswordValidator/PasswordValidator.svelte
@@ -0,0 +1,18 @@
+<div class="field">
+  <p class="control">
+    <label for="" class="label">Password</label>
+    <input
+      class="input {password && !is_valid ? 'is-danger' : ''}"
+      type="password"
+      required
+      bind:value={password}
+      autocomplete="off" />
+    <KwsPasswordValidator {password} bind:is_valid />
+  </p>
+</div>
+
+<script>
+  import { PasswordValidator as KwsPasswordValidator } from "@kws3/ui";
+  export let password = "",
+    is_valid = false;
+</script>
