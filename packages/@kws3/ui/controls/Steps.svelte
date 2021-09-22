71b6481d6f7b7aa4381ebb2db315028aa1c6d063
Sabir <sabirveli@gmail.com>
2021-09-22 18:29:27 +0530

    Steps circle multi design

--- packages/@kws3/ui/controls/Steps.svelte
+++ packages/@kws3/ui/controls/Steps.svelte
@@ -1,16 +1,37 @@
+<!--
+  @component
+
+
+  @param {array} [steps=[]] - Object, which contains name, description, custom value and custom icon of each steps, Default: `[]`
+  @param {number} [active_index=0] - Index used to determine which step needs to be active, Default: `0`
+  @param {''|'small'|'medium'|'large'} [size=""] - Size of the step, Default: `""`
+  @param {''|'primary'|'warning'|'success'|'info'|'danger'|'dark'|'light'} [color=""] - Color of the step, Default: `""`
+  @param {boolean} [clickable=false] - Determine, can able to navigate through steps, Default: `false`
+  @param {boolean} [icon_only=false] - Show step icon given in `step_icon` property instead of numbers, Default: `false`
+  @param {string} [step_icon="check"] - Icon used to displayed in steps, Default: `"check"`
+  @param {boolean} [have_step_value=true] - Determine the step circle needs value.
+If its `false`, it doesn't display number, icon or character, Default: `true`
+  @param {boolean} [hollow_step=true] - Determine the step circle design is hollow or filled, Default: `true`
+  @param {string} [class=""] - CSS classes for the steps container, Default: `""`
+
+-->
 <ul class="steps {klass} is-{size}">
   {#each steps as step, idx}
     <li
-      class="step-item is-{color} {active_index >= idx ? 'is-active' : ''}"
+      class="step-item is-{color} {active_index >= idx && hollow_step
+        ? 'is-active'
+        : ''} {active_index >= idx && !hollow_step ? 'is-completed' : ''}"
       on:click={() => navigateStep(idx)}
       style={clickable ? "cursor:pointer;" : ""}>
       <div class="step-marker">
-        {#if icon_only}
-          <Icon icon={step.step_icon ? step.step_icon : step_icon} />
-        {:else if step.value}
-          {step.value}
-        {:else}
-          {idx + 1}
+        {#if have_step_value}
+          {#if icon_only}
+            <Icon icon={step.step_icon ? step.step_icon : step_icon} />
+          {:else if step.value}
+            {step.value}
+          {:else}
+            {idx + 1}
+          {/if}
         {/if}
       </div>
       {#if step.name || step.description}
@@ -33,7 +54,7 @@
    */
   export let steps = [],
     /**
-     * Index used to determine which step needs to be `active`
+     * Index used to determine which step needs to be active
      */
     active_index = 0,
     /**
@@ -57,7 +78,16 @@
     /**
      * Icon used to displayed in steps
      */
-    step_icon = "check";
+    step_icon = "check",
+    /**
+     * Determine the step circle needs value.
+     * If its `false`, it doesn't display number, icon or character
+     */
+    have_step_value = true,
+    /**
+     * Determine the step circle design is hollow or filled
+     */
+    hollow_step = true;
 
   /**
    * CSS classes for the steps container
