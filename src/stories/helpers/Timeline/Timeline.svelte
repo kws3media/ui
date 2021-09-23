ece475cd073007c1a36a3feb4de77b805a12348a
Sabir <sabirveli@gmail.com>
2021-09-23 17:23:35 +0530

    Timeline story

---
+++ src/stories/helpers/Timeline/Timeline.svelte
@@ -0,0 +1,18 @@
+<KwsTimeline {klass} {data} />
+
+<script>
+  import { Timeline as KwsTimeline } from "@kws3/ui";
+  export let data = [
+    {
+      user: "User 1",
+      description: "Test",
+    },
+    {
+      user: "User 2",
+      description: "Test 2",
+    },
+  ];
+
+  let klass = "is-primary";
+  export { klass as class };
+</script>
