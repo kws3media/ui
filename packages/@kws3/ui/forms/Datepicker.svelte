ab35c5cfac2ffea5d7231b1bb01209efd6c55f50
Suneesh S K <suneeshsk3@gmail.com>
2021-09-29 20:40:12 +0530

    keep any type

--- packages/@kws3/ui/forms/Datepicker.svelte
+++ packages/@kws3/ui/forms/Datepicker.svelte
@@ -12,12 +12,12 @@ This property can be bound to, to fetch the selected date or date range. Output 
   @param {boolean} [disabled=false] - Disables the component, Default: `false`
   @param {string} [placeholder="Select Date.."] - Placeholder text for the input, Default: `"Select Date.."`
   @param {'primary'|'warning'|'info'|'danger'|'dark'|'light'} [calendar_color="primary"] - Colour of the Calendar, Default: `"primary"`
-  @param {string} [min_date=""] - Set earliest selectable date as an object or string
+  @param {any} [min_date=] - Set earliest selectable date as an object or string
 
-**Example:** `'2021-06-06'` or `{(new Date('2021-01-01'))}`, Default: `""`
-  @param {string} [max_date=""] - Set latest selectable date as an object or string
+**Example:** `'2021-06-06'` or `{(new Date('2021-01-01'))}`, Default: ``
+  @param {any} [max_date=] - Set latest selectable date as an object or string
 
-**Example:** `'2021-06-06'` or `{(new Date('2021-01-01'))}`, Default: `""`
+**Example:** `'2021-06-06'` or `{(new Date('2021-01-01'))}`, Default: ``
   @param {array} [enable_dates=[]] - Enables a range of dates and disables all others, Default: `[]`
   @param {array} [disable_dates=[]] - Disables a range of dates and enables all others, Default: `[]`
   @param {boolean} [range_mode=false] - Allows selecting a date range, Default: `false`
@@ -94,14 +94,14 @@ See: https://flatpickr.js.org/options/, Default: `{}`
    * Set earliest selectable date as an object or string
    *
    * **Example:** `'2021-06-06'` or `{(new Date('2021-01-01'))}`
-   * @type {string}
+   * @type {any}
    */
   export let min_date = "";
   /**
    * Set latest selectable date as an object or string
    *
    * **Example:** `'2021-06-06'` or `{(new Date('2021-01-01'))}`
-   * @type {string}
+   * @type {any}
    */
   export let max_date = "";
   /**
