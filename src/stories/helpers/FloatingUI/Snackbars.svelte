052554aca4b9cfaa6f47ddae61cbbab660e274da
ekhaled <me.khaled@gmail.com>
2021-09-29 12:39:52 +0100

    fill docs

---
+++ src/stories/helpers/FloatingUI/Snackbars.svelte
@@ -0,0 +1,64 @@
+<div class="columns">
+  <div class="column has-text-centered">
+    <button class="button is-primary" type="button" on:click={push}
+      >Create Snackbar</button>
+  </div>
+</div>
+
+<script>
+  import { Snackbars as KWSSnack } from "@kws3/ui";
+
+  /**
+   * Title/Heading of the snackbar
+   */
+  export let title = "",
+    /**
+     * Message in the snackbar
+     */
+    message = "",
+    /**
+     * Duration of the snackbar
+     */
+    duration = 3000,
+    /**
+     * Toast snackbar background color
+     * @type {''|'primary'|'success'|'warning'|'info'|'danger'|'dark'|'light'}
+     */
+    color = "primary",
+    /**
+     * Position of the snackbar. `""` defaults to `bottom-right`. Can be modified globally in framework settings
+     *
+     * @type {''|'top'|'bottom'|'top-left'|'top-right'|'bottom-left'|'bottom-right'}
+     */
+    position = "bottom-right",
+    /**
+     * Light color variant for snackbar box and contents
+     */
+    light = false,
+    /**
+     * A persistent snackbar without duration, does not auto-dismiss after `duration` has elapsed
+     */
+    persistent = false,
+    /**
+     * A Custom value that can be passed in to the Snackbar, useful for identification purposes.
+     */
+    context = 1;
+
+  let count = 1;
+
+  const push = () => {
+    let { props, destroy } = KWSSnack.create({
+      title,
+      duration,
+      color,
+      position,
+      dismissable,
+      persistent,
+      context,
+      light,
+      message: count + " - " + message,
+    });
+
+    count++;
+  };
+</script>
