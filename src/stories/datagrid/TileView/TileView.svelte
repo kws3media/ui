cc33f5ae4362ba41a5095001cf9dd617239f20a6
Sabir <sabirveli@gmail.com>
2021-09-30 21:39:29 +0530

    TileView WIP

---
+++ src/stories/datagrid/TileView/TileView.svelte
@@ -0,0 +1,38 @@
+<KwsTileView
+  {iteration_key}
+  {data}
+  {transition}
+  {tileItemComponent}
+  {per_row}
+  {columns}
+  {clickableRows}
+  {bulk_actions}
+  {selectAll}
+  {selectedIds}
+  {selectCheckboxColor}
+  {selectCheckboxSize}
+  {valueTransformers}
+  {classTransformers}
+  {styleTransformers}
+  {visibilityMap} />
+
+<script>
+  import { TileView as KwsTileView } from "@kws3/ui";
+
+  export let iteration_key = "id",
+    data = [],
+    transition = false,
+    tileItemComponent = null,
+    per_row = 3,
+    columns = {},
+    clickableRows = false,
+    bulk_actions = false,
+    selectAll = false,
+    selectedIds = [],
+    selectCheckboxColor = "info",
+    selectCheckboxSize = "medium",
+    valueTransformers = {},
+    classTransformers = {},
+    styleTransformers = {},
+    visibilityMap = {};
+</script>
