dc3b1786204c39dfd3b9592656c519c251bf3356
ekhaled <me.khaled@gmail.com>
2021-11-11 12:32:48 +0000

    update docs

--- packages/@kws3/ui/forms/PasswordValidator/PasswordValidator.svelte
+++ packages/@kws3/ui/forms/PasswordValidator/PasswordValidator.svelte
@@ -2,9 +2,25 @@
   @component
 
 
+  @param {number} [min=8] - Minimum number of characters required, Default: `8`
+  @param {boolean} [lower=true] - Whether a lower-case character is required, Default: `true`
+  @param {boolean} [upper=true] - Whether a upper-case character is required, Default: `true`
+  @param {boolean} [digit=true] - Whether a digit character is required, Default: `true`
+  @param {boolean} [special=true] - Whether a special character is required, Default: `true`
+  @param {array} [custom_rules=[]] - Array of custom rules defined as objects
+
+Object must contain:
+`name`
+`text`
+`identifier`
+`regex`
+`negate (optional)`, Default: `[]`
   @param {string} [password=""] - Password for validating, Default: `""`
   @param {string} [style=""] - Inline CSS style for container, Default: `""`
-  @param {boolean} [is_valid=false] - Variable used to check if password Validator returns true or false, Default: `false`
+  @param {boolean} [compact=true] - Determines whether validator should display in compact mode or expanded mode, Default: `true`
+  @param {boolean} [valid=false] - Variable used to check if password Validator returns true or false
+
+Should be used with `bind` from parent component, Default: `false`
   @param {string} [class=""] - CSS classes for container, Default: `""`
 
 -->
