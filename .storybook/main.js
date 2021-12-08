c037d924f576951faf3debde8340bb3082a25a73
Suneesh S K <suneeshsk3@gmail.com>
2021-12-08 17:25:52 +0530

    package update

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,6 +14,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-actions",
     "@storybook/addon-viewport",
+    "@storybook/addon-interactions",
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({
