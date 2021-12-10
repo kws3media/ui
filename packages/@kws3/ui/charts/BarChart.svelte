365c3b7259b0606081f464d05ee7c1de2d02f707
ekhaled <me.khaled@gmail.com>
2021-12-10 14:48:30 +0000

    prelim version of bar chart

---
+++ packages/@kws3/ui/charts/BarChart.svelte
@@ -0,0 +1,181 @@
+<!--
+  @component
+
+
+  @param {object} [options={}] - Chart options, see ApexCharts options for bar charts, Default: `{}`
+  @param {array} [labels=[]] - Data labels, Default: `[]`
+  @param {array} [sets=[]] - Data sets, Default: `[]`
+  @param {array} [data=[]] - Chart data, Default: `[]`
+  @param {string} [width="100%"] - Chart width, supports CSS size strings, Default: `"100%"`
+  @param {string} [height="auto"] - Chart height, supports CSS size strings, Default: `"auto"`
+  @param {array} [colors=null] - Chart colors, can be modified globally in framework settings
+
+Send an array of colors to override the default colors, or do not send anything to use the default colors, Default: `null`
+  @param {string} [class=""] - CSS classes for container, Default: `""`
+
+-->
+<Chart
+  class={__class}
+  options={_options}
+  series={_data}
+  type="bar"
+  {height}
+  {width} />
+
+<script>
+  import { Chart } from "@kws3/ui";
+  import { barChartOptions, merge } from "./utils";
+  import { defaultChartColors } from "../settings";
+
+  /**
+   * Chart options, see ApexCharts options for bar charts
+   */
+  export let options = {},
+    /**
+     * Data labels
+     */
+    labels = [],
+    /**
+     * Data sets
+     */
+    sets = [],
+    /**
+     * Chart data
+     */
+    data = [],
+    /**
+     * Chart width, supports CSS size strings
+     */
+    width = "100%",
+    /**
+     * Chart height, supports CSS size strings
+     */
+    height = "auto",
+    /**
+     * Chart colors, can be modified globally in framework settings
+     *
+     * Send an array of colors to override the default colors, or do not send anything to use the default colors
+     * @type {array}
+     */
+    colors = null;
+
+  /**
+   * CSS classes for container
+   */
+  let klass = "";
+  export { klass as class };
+
+  $: __class =
+    "kws-bar-chart " + `${sparklines ? "kws-sparklines" : ""} ` + klass;
+
+  let _data = [];
+  let yAxis = [];
+
+  let sparklines = false;
+
+  $: usedColors = colors
+    ? colors
+    : $defaultChartColors
+    ? $defaultChartColors
+    : [];
+
+  $: data, sets, normaliseAll();
+  $: _options = merge(
+    barChartOptions(labels, yAxis[0], sparklines),
+    Object.assign({}, { colors: usedColors }, options)
+  );
+
+  const normaliseAll = () => {
+    if (data.length !== sets.length) {
+      throw new Error("Data and sets lengths do not match");
+    }
+    normaliseSets();
+    normaliseData();
+  };
+
+  const normaliseSets = () => {
+    yAxis = [];
+
+    let _sets = sets || [],
+      oppositeAxisApplied = false,
+      mainSeriesName = null,
+      oppositeSeriesName = null;
+
+    _sets.forEach((set, idx) => {
+      let __set = {};
+      let obj = {
+        floating: false,
+        opposite: false,
+        decimalsInFloat: 0,
+        axisBorder: {
+          show: true,
+        },
+        axisTicks: {
+          show: true,
+        },
+        labels: {
+          show: true,
+        },
+      };
+      if (typeof set === "string") {
+        __set = Object.assign(obj, {
+          name: set,
+          type: "column",
+        });
+      } else {
+        __set = Object.assign(obj, { name: "set-" + idx, type: "column" }, set);
+      }
+
+      //series names cannot be random,
+      //they have to be of one of the sets
+      if (!mainSeriesName) {
+        mainSeriesName = __set.name;
+      }
+
+      if (__set.opposite) {
+        //hide extras axes on the opposite side
+        if (!oppositeAxisApplied) {
+          oppositeAxisApplied = true;
+          if (!oppositeSeriesName) {
+            oppositeSeriesName = __set.name;
+          }
+        } else {
+          __set.show = false;
+        }
+      } else {
+        //hide extras axes on the main side
+        if (idx > 0) {
+          __set.show = false;
+        }
+      }
+
+      if (__set.forceDisplay) {
+        __set.show = true;
+      }
+
+      //set a common series name based on axis..
+      //so that scales for each axis are the same
+      __set.seriesName = __set.opposite ? oppositeSeriesName : mainSeriesName;
+
+      yAxis.push(__set);
+    });
+  };
+
+  const normaliseData = () => {
+    _data = [];
+
+    //ensuring data is an array of arrays
+    let sliced_data = (data || []).slice();
+    if (!Array.isArray(sliced_data[0])) {
+      sliced_data = [sliced_data];
+    }
+
+    sliced_data.forEach((set, idx) => {
+      _data.push({
+        name: yAxis[idx].name,
+        type: yAxis[idx].type,
+        data: set,
+      });
+    });
+  };
+</script>
