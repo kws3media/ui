26199315fddc5189a63897447d24e8a335859b19
ekhaled <me.khaled@gmail.com>
2021-12-13 17:00:54 +0000

    upgrade and loading bugfixes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,13 @@
 const path = require("path");
+
 const autoPreprocess = require("svelte-preprocess");
 
 module.exports = {
-  stories: ["../src/stories/index.stories.js", "../src/stories/**/*.stories.*"],
+  stories: [
+    "../src/stories/_index/index.stories.mdx",
+    "../src/stories/index.stories.js",
+    "../src/stories/**/*.stories.*",
+  ],
   logLevel: "debug",
   features: {
     storyStoreV7: true,
@@ -26,14 +31,12 @@ module.exports = {
       include: path.resolve(__dirname, "../src"),
       use: ["style-loader", "css-loader", "sass-loader"],
     });
-
     const svelteLoader = config.module.rules.find(
       (r) => r.loader && r.loader.includes("svelte-loader")
     );
     svelteLoader.options.preprocess = autoPreprocess({});
-
     config.resolve.modules.push(path.resolve("./packages"));
-
     return config;
   },
+  framework: "@storybook/svelte",
 };
