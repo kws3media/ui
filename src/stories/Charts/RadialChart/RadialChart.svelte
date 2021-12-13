5ab8bf40a5f16364e91de967d168999852a819ee
ekhaled <me.khaled@gmail.com>
2021-12-13 16:35:21 +0000

    radial charts

---
+++ src/stories/Charts/RadialChart/RadialChart.svelte
@@ -0,0 +1,24 @@
+<Chart
+  {options}
+  {colors}
+  {data}
+  {labels}
+  {start_angle}
+  {end_angle}
+  {sparklines}
+  {width}
+  {height} />
+
+<script>
+  import { RadialChart as Chart } from "@kws3/ui";
+
+  export let options = {},
+    colors = null,
+    data = [],
+    labels = [],
+    start_angle = 0,
+    end_angle = 360,
+    sparklines = false,
+    width = "100%",
+    height = "auto";
+</script>
