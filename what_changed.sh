288c997285f1871953901b63bc360853deae339c
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-01-21 16:14:01 +0600

    only changes happend in ui folder

--- what_changed.sh
+++ what_changed.sh
@@ -3,4 +3,4 @@ package_path="./packages/@kws3/ui/package.json";
 pkg_version="$(node -p "require('$package_path').version")"
 latest_tag="@kws3/ui@${pkg_version}"
 # git log $latest_tag..HEAD  --oneline
-git log $latest_tag..HEAD  --oneline --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit
+git log $latest_tag..HEAD  --oneline --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit  -- packages/@kws3/ui/*
