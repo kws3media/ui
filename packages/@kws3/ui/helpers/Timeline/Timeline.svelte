47e265760fa9fbcb52dcf5fcdfd66fbbccbf8f78
ekhaled <me.khaled@gmail.com>
2022-01-21 19:11:29 +0000

    add default slot

--- packages/@kws3/ui/helpers/Timeline/Timeline.svelte
+++ packages/@kws3/ui/helpers/Timeline/Timeline.svelte
@@ -5,8 +5,13 @@
   @param {'left'|'center'|'right'} [align="left"] - Alignment of the Timeline child items, Default: `"left"`
   @param {string} [class=""] - CSS classes for the Timeline container, Default: `""`
 
+  ### Slots
+  - `<slot name="default"  />` - Default slot for placing all TimelineItem and TimelineHeader components
+
 -->
-<ul class="kws-timeline is-{align} {klass}" />
+<ul class="kws-timeline is-{align} {klass}">
+  <!--Default slot for placing all TimelineItem and TimelineHeader components--><slot />
+</ul>
 
 <script>
   /**
