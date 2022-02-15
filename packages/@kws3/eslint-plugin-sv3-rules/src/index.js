cec79f54e4a6878055cc8a0f759d2a76d8eebf48
ekhaled <me.khaled@gmail.com>
2022-02-15 17:54:02 +0000

    also export configs and overrides

--- packages/@kws3/eslint-plugin-sv3-rules/src/index.js
+++ packages/@kws3/eslint-plugin-sv3-rules/src/index.js
@@ -1,9 +1,27 @@
 import noDateStringArg from "./rules/no-date-string-arg";
 import noDatalistScaffold from "./rules/no-datalist-scaffold";
 
-const rules = [noDateStringArg, noDatalistScaffold].reduce((obj, r) => {
+const allRules = [noDateStringArg, noDatalistScaffold];
+
+const configs = {
+  base: {
+    plugins: ["@kws3/eslint-plugin-sv3-rules"],
+    overrides: [
+      {
+        files: ["*.svelte"],
+        parser: require.resolve("svelte-eslint-parser"),
+        rules: allRules.reduce((obj, r) => {
+          obj[r.meta.docs.ruleName] = "error";
+          return obj;
+        }, {}),
+      },
+    ],
+  },
+};
+
+const rules = allRules.reduce((obj, r) => {
   obj[r.meta.docs.ruleName] = r;
   return obj;
 }, {});
 
-export default { rules };
+export default { configs, rules };
