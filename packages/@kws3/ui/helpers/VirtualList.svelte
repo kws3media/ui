7ad67fdf472d60cca6cc481cecaf7b2f213d7127
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-02-28 16:51:52 +0600

    initial component structure

---
+++ packages/@kws3/ui/helpers/VirtualList.svelte
@@ -0,0 +1,15 @@
+<div
+  bind:this={element}
+  class="kws-virtual-list"
+  on:scroll={() => refresh()}
+  style:height
+  bind:offsetHeight={viewportHeight} />
+
+<script>
+  export let height;
+
+  let element, //whole wrapping element
+    viewportHeight; //height of the viewport
+
+  function refresh() {}
+</script>
