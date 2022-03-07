219a43f74f4b8a633faa4a027d79321ad2000441
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-03-07 20:51:48 +0600

    story wrapper

---
+++ src/stories/helpers/VirtualList/VirtualList.svelte
@@ -0,0 +1,12 @@
+<div class="p-4" style="width:500px;margin:0 auto;">
+  <KwsVirtualList items={users} itemHeight={54} bind:start bind:end />
+</div>
+
+<script>
+  const response = require("./users.json");
+  import { VirtualList as KwsVirtualList } from "@kws3/ui";
+
+  let users = response.records;
+  let start = 0;
+  let end = users.length;
+</script>
