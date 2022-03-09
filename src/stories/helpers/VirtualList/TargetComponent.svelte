a665082d2e285c7e8a14c57e5a36ff7b34e7fc94
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-03-09 17:00:27 +0600

    initial target component

---
+++ src/stories/helpers/VirtualList/TargetComponent.svelte
@@ -0,0 +1,33 @@
+<div class="item">
+  <div class="level is-mobile">
+    <div class="level-left">
+      <span class="name">
+        {name}
+        {surname}
+      </span>
+    </div>
+    <div class="level-right">
+      <span>
+        {username}
+      </span>
+    </div>
+  </div>
+  <div class="level is-mobile">
+    <div class="level-left">
+      <span class="desc">{role}</span>
+    </div>
+    <div class="level-right">
+      <span>
+        {id}
+      </span>
+    </div>
+  </div>
+</div>
+
+<script>
+  export let name = "";
+  export let surname = "";
+  export let username = "";
+  export let role = "";
+  export let id = "";
+</script>
