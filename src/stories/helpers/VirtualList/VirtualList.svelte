e8b3c3784d14026dcfa2e62fbcaf6b8a52e7184d
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-03-24 19:31:42 +0600

    increase item counts

--- src/stories/helpers/VirtualList/VirtualList.svelte
+++ src/stories/helpers/VirtualList/VirtualList.svelte
@@ -1,8 +1,8 @@
-<p class="has-text-centered">showing items {start}-{end} of {items.length}</p>
+<p class="has-text-centered">showing items {start}-{end} of {_items.length}</p>
 <div style="height:700px;width:90vw; border:1px solid rgba(0,0,0,0.1)">
   <KwsVirtualList
-    Component={TargetComponent}
-    {items}
+    {Component}
+    items={_items}
     bind:start
     bind:end
     clickableRows={true}
@@ -11,18 +11,21 @@
 
 <script>
   import { VirtualList as KwsVirtualList } from "@kws3/ui";
-  import TargetComponent from "./TargetComponent.svelte";
   import data from "./data.json";
 
-  export let items = data.records;
+  let records = data.records;
+  export let _items = new Array(100)
+    .fill()
+    .map(() => records[Math.floor(Math.random() * records.length)]);
 
-  let start = 0,
+  export let Component = null,
+    start = 0,
     end = 0;
 
   $: {
-    if (end === items.length) {
-      let itemsToAdd = items.sort(() => 0.5 - Math.random()).slice(0, 5); // get random 5 items
-      items = [...items, ...itemsToAdd];
+    if (end === _items.length) {
+      let itemsToAdd = _items.sort(() => 0.5 - Math.random()).slice(0, 20); // get random 20 items
+      _items = [..._items, ...itemsToAdd];
     }
   }
 
