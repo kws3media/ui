ce76943f60107490a0c6c8f14a4800de5bde33df
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-03-28 20:15:41 +0600

    LOADER SIZE FIX

--- packages/@kws3/ui/forms/select/MultiSelect.svelte
+++ packages/@kws3/ui/forms/select/MultiSelect.svelte
@@ -103,7 +103,7 @@ Default value: `<span>{option[search_key] || option}</span>`
       role="button"
       type="button"
       style="border: none;"
-      class="button is-small is-loading" />
+      class="button is-paddingless delete is-small is-loading" />
   {:else if !readonly && !disabled}
     <button
       role="button"
