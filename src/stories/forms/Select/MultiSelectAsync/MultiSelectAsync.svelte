307071fe44e18472ade7e7f51e4813b9d5401151
ekhaled <me.khaled@gmail.com>
2022-04-06 17:34:57 +0100

    multiselect async stories

---
+++ src/stories/forms/Select/MultiSelectAsync/MultiSelectAsync.svelte
@@ -0,0 +1,105 @@
+<div id="ms_container">
+  <div class="columns">
+    <div class="column is-6">
+      <Panel title="Input data" collapsible collapsed>
+        <pre>{JSON.stringify(brands, null, 1)}</pre>
+      </Panel>
+    </div>
+    <div class="column is-6">
+      <KwsMultiSelect
+        {size}
+        {color}
+        {max}
+        search={fetchObjBrandsHelper}
+        bind:value={selected_brand}
+        search_key="name"
+        value_key="id"
+        {placeholder}
+        {style}
+        class={klass}
+        {disabled}
+        {readonly}
+        {summary_mode}
+        {selected_icon}
+        {no_options_msg}
+        {remove_all_tip}
+        {remove_btn_tip} />
+      Output data:
+      <code>{JSON.stringify(selected_brand, null)}</code>
+    </div>
+  </div>
+
+  <hr />
+  <div class="columns">
+    <div class="column is-6">
+      <Panel title="Input data" collapsible collapsed>
+        <pre>{JSON.stringify(brands_string_only, null, 1)}</pre>
+      </Panel>
+    </div>
+    <div class="column is-6">
+      <KwsMultiSelect
+        {size}
+        {color}
+        {max}
+        search={fetchBrands}
+        bind:value={selected_brand_string}
+        {search_key}
+        {value_key}
+        {placeholder}
+        {style}
+        class={klass}
+        {disabled}
+        {readonly}
+        {summary_mode}
+        {selected_icon}
+        {no_options_msg}
+        {remove_all_tip}
+        {remove_btn_tip} />
+      Output data:
+      <code>{JSON.stringify(selected_brand_string, null, 2)}</code>
+    </div>
+  </div>
+</div>
+
+<script>
+  import {
+    Panel,
+    MultiSelect as KwsMultiSelect,
+    activateTooltips,
+  } from "@kws3/ui";
+  import { onMount } from "svelte";
+
+  import {
+    brands,
+    brands_string_only,
+    fetchBrands,
+    fetchObjBrands,
+  } from "../data.js";
+
+  onMount(() => activateTooltips("#ms_container"));
+
+  export let size = "",
+    color = "",
+    style = "",
+    disabled = false,
+    readonly = false,
+    max = null,
+    search_key = "name",
+    value_key = "id",
+    selected_icon = "check",
+    no_options_msg = "No matching options",
+    remove_all_tip = "Remove all",
+    remove_btn_tip = "Remove",
+    placeholder = "Choose a Brand...",
+    summary_mode = false,
+    selected_brand = [], // single , also work with { id: 10, name: "Blackberry" }
+    //selected_brand_multi = "", // max item null, not works if string given. should work?
+    selected_brand_string = ["Blackberry"];
+
+  const fetchObjBrandsHelper = async function (filter) {
+    return await fetchObjBrands(filter, search_key);
+  };
+
+  let klass = "";
+  export { klass as class };
+</script>
