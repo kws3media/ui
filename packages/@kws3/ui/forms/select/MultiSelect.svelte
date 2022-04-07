b8a5d8b3c8359a86add576e6607d88615092cb52
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-04-07 16:47:11 +0600

    fix

--- packages/@kws3/ui/forms/select/MultiSelect.svelte
+++ packages/@kws3/ui/forms/select/MultiSelect.svelte
@@ -20,7 +20,7 @@ this property of each object will be returned as the value, Default: `"id"`
 
 Only send this prop if you want to fetch `options` asynchronously.
 `options` prop will be ignored if this prop is set., Default: `null`
-  @param {'strict'|'fuzzy'} [search_strategy="strict"] - Determining options to return strictly based on search text or perform a fuzzy match
+  @param {'strict'|'fuzzy'} [search_strategy="strict"] - Determining options to return strictly based on search text or perform a fuzzy match.
 Fuzzy match will not work if `search` function is set., Default: `"strict"`
   @param {''|'small'|'medium'|'large'} [size=""] - Size of the input, Default: `""`
   @param {''|'primary'|'success'|'warning'|'info'|'danger'|'dark'|'light'} [color=""] - Color of the input, Default: `""`
@@ -226,7 +226,7 @@ Default value: `<span>{option[search_key] || option}</span>`
   export let search = null;
 
   /**
-   * Determining options to return strictly based on search text or perform a fuzzy match
+   * Determining options to return strictly based on search text or perform a fuzzy match.
    * Fuzzy match will not work if `search` function is set.
    * @type {'strict'|'fuzzy'}
    */
