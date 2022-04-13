a50a2f4973e60c14a10191cb58dfc3cbe43fd2e8
ekhaled <me.khaled@gmail.com>
2022-04-13 17:52:23 +0100

    simulate network loading

--- src/stories/helpers/VirtualList/VirtualList.svelte
+++ src/stories/helpers/VirtualList/VirtualList.svelte
@@ -1,13 +1,18 @@
 <div class="columns">
   <div class="column is-narrow">
     <p class="has-text-centered">
-      showing items {start}-{end} of {_items.length}
+      Showing items {start}-{end} of {_items.length}
     </p>
     <div
-      style="height:{height}px;width:500px; border:1px solid rgba(0,0,0,0.1)">
+      style="height:{height}px;width:500px; border:1px solid rgba(0,0,0,0.1); position:relative;">
       <KwsVirtualList items={_items} bind:start bind:end let:item let:index>
         <TargetComponent {item} {index} on:rowClick={rowClicked} />
       </KwsVirtualList>
+      {#if loading}
+        <span
+          class="button is-text is-large is-loading"
+          style="position:absolute; bottom:0;left:245px;" />
+      {/if}
     </div>
   </div>
   <div class="column">
@@ -27,11 +32,13 @@
 <script>
   import { VirtualList as KwsVirtualList } from "@kws3/ui";
   import TargetComponent from "./TargetComponent.svelte";
+  import { sleep } from "../../../utils";
   import data from "./data.json";
 
   let records = data.records;
   let height = 600,
-    heights = [800, 600, 500, 300, 100];
+    heights = [800, 600, 500, 300, 100],
+    loading = false;
   export let _items = new Array(100)
     .fill()
     .map(() => records[Math.floor(Math.random() * records.length)]);
@@ -41,10 +48,17 @@
 
   $: {
     if (end === _items.length) {
-      let itemsToAdd = _items.sort(() => 0.5 - Math.random()).slice(0, 20); // get random 20 items
-      _items = [..._items, ...itemsToAdd];
+      loadMore();
     }
   }
+
+  const loadMore = async () => {
+    loading = true;
+    await sleep(1000);
+    let itemsToAdd = _items.sort(() => 0.5 - Math.random()).slice(0, 20); // get random 20 items
+    _items = [..._items, ...itemsToAdd];
+    loading = false;
+  };
 
   function rowClicked(e) {
     console.log(e.detail);
