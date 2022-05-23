69251b7d458f044483342bfda1bf22033f27b586
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-05-23 18:13:40 +0600

    basic test run and covered from test plan

--- src/stories/forms/AutoComplete/AutoComplete/autocomplete.play.js
+++ src/stories/forms/AutoComplete/AutoComplete/autocomplete.play.js
@@ -1,8 +1,65 @@
-/* eslint-disable no-unused-vars */
 import { expect } from "@storybook/jest";
-import { within, userEvent, waitFor } from "@storybook/testing-library";
+import {
+  within,
+  userEvent,
+  // eslint-disable-next-line no-unused-vars
+  waitFor,
+  getNodeText,
+  fireEvent,
+} from "@storybook/testing-library";
 import { sleep } from "../../../../utils";
 
+// eslint-disable-next-line no-unused-vars
 export default async ({ args, canvasElement }) => {
+  // eslint-disable-next-line no-unused-vars
   const canvas = within(canvasElement);
+  const inputs = canvasElement.querySelectorAll("input.input");
+
+  //Delay play
+  await sleep(1000);
+
+  //Focus on Multi-select
+  await userEvent.click(inputs[0]);
+
+  const options = document.querySelectorAll("ul.options"),
+    items = options[0].getElementsByTagName("li");
+
+  //No dropdown should be visible until user types and found matches
+  await expect(options[0].classList.contains("hidden")).toEqual(true);
+
+  await sleep(300);
+  await userEvent.type(inputs[0], "tony ", { delay: 100 });
+  await userEvent.type(inputs[0], "roger", { delay: 100 });
+  await expect(Number(items.length)).toBe(2);
+  await expect(HTMLtoString(items[0].innerHTML).toLowerCase()).toBe(
+    "tony stark"
+  );
+  await expect(HTMLtoString(items[1].innerHTML).toLowerCase()).toBe(
+    "steve rogers"
+  );
+
+  // on enter options should be empty
+  await sleep(300);
+  await fireEvent.keyDown(inputs[0], { key: "Enter" });
+  await expect(Number(items.length)).toEqual(0);
+  await expect(inputs[0].value).toEqual("tony roger");
+
+  await sleep(300);
+  await userEvent.type(inputs[0], " banner", { delay: 100 });
+  await expect(Number(items.length)).toBe(3);
+  await expect(HTMLtoString(items[2].innerHTML).toLowerCase()).toBe(
+    "bruce banner"
+  );
+
+  await sleep(300);
+  inputs[0].value = "";
+  await userEvent.type(inputs[0], "drax", { delay: 100 });
+  await expect(Number(items.length)).toBe(1);
+  await expect(HTMLtoString(items[0].innerHTML).toLowerCase()).toBe("drax");
+  await fireEvent.keyDown(inputs[0], { key: "Enter" });
+  await expect(Number(items.length)).toEqual(0);
 };
+
+function HTMLtoString(html) {
+  return html.replace(/<[^>]*>/g, "").trim();
+}
--- src/stories/forms/AutoComplete/AutoComplete/testplan.md
+++ src/stories/forms/AutoComplete/AutoComplete/testplan.md
@@ -1,1 +1,18 @@
 ### AutoComplete
+### We will use the pre-populated values for testing
+
+### Focus response
+1. Focus on AutoComplete.
+ - Result: No dropdown should be visible until user types and found matches.
+
+### Testing search function
+1. Test: Type “tony” (with space) and "roger".
+ - Result: The dropdown should display “tony stark” and "steve rogers".
+2. Test: Press “Enter”.
+ - Result: The dropdown should disappear and input value should be “tony roger".
+3. Type “banner” again (with pre space).
+ - Result: The dropdown should display “tony stark”, "steve rogers" and "bruce banner".
+4. Empty input then type “drax”.
+  - Result: The dropdown should display “drax”.
+5. Press “Enter”.
+  - Result: The dropdown should disappear and input value should be “drax”.
