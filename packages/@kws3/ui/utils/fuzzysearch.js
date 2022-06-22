456b8cba55f4699183e918145deccce9b14a3ea4
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-06-22 19:54:10 +0600

    initial fuzzysearch composer

--- packages/@kws3/ui/utils/fuzzysearch.js
+++ packages/@kws3/ui/utils/fuzzysearch.js
@@ -1,20 +1,32 @@
-export default function fuzzysearch(needle, haystack) {
-  var tlen = haystack.length;
-  var qlen = needle.length;
-  if (qlen > tlen) {
-    return false;
-  }
-  if (qlen === tlen) {
-    return needle === haystack;
-  }
-  outer: for (var i = 0, j = 0; i < qlen; i++) {
-    var nch = needle.charCodeAt(i);
-    while (j < tlen) {
-      if (haystack.charCodeAt(j++) === nch) {
-        continue outer;
-      }
-    }
-    return false;
-  }
-  return true;
+import fuzzy from "fuzzy.js";
+
+export default function fuzzysearch(
+  needle,
+  haystack,
+  search_key = "value",
+  scoreThreshold = 5
+) {
+  let OPTS = haystack.map((item) => {
+    let key =
+      typeof item === "object" && `${search_key}` in item
+        ? item[search_key]
+        : item;
+    let output = fuzzy(key, needle);
+
+    item = { ...output, original: item };
+    item.score =
+      !item.score || (item.score && item.score < output.score)
+        ? output.score
+        : item.score || 0;
+    return item;
+  });
+
+  let maxScore = Math.max(...OPTS.map((i) => i.score));
+  let calculatedLimit = maxScore - scoreThreshold;
+
+  OPTS = OPTS.filter(
+    (r) => r.score > (calculatedLimit > 0 ? calculatedLimit : 0)
+  );
+
+  return OPTS;
 }
