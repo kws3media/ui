6071b78e84fca188925ffdfa84334cf53b7af0fe
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-06-27 18:35:21 +0600

    support for multiple keys

--- packages/@kws3/ui/utils/fuzzysearch.js
+++ packages/@kws3/ui/utils/fuzzysearch.js
@@ -1,8 +1,23 @@
 import fuzzy from "fuzzy.js";
 
 export function fuzzysearch(needle, haystack, opts) {
+  console.log("calling fuzzy", { needle, haystack, opts });
   let search_key = defaultValue(opts, "search_key", "value");
   let scoreThreshold = defaultValue(opts, "scoreThreshold", 5);
+  let result = {};
+
+  if (!Array.isArray(search_key)) {
+    search_key = [search_key];
+  }
+
+  search_key.forEach((s_key) => {
+    result[s_key] = searchHandler(s_key, needle, haystack, scoreThreshold);
+  });
+
+  return result;
+}
+
+function searchHandler(search_key, needle, haystack, scoreThreshold) {
   let OPTS = haystack.map((item) => {
     let key =
       typeof item === "object" && `${search_key}` in item
