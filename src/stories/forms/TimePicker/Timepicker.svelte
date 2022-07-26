cf710fe6d0a03a4c8a780912eba28ffd0e014f7b
Sabir SM <sabirveli@gmail.com>
2022-07-26 16:04:12 +0530

    more props for checking

--- src/stories/forms/TimePicker/Timepicker.svelte
+++ src/stories/forms/TimePicker/Timepicker.svelte
@@ -5,35 +5,54 @@
         {style}
         {placeholder}
         {disabled}
-        bind:time_24hr
         {options}
         {ui_color}
         bind:value
+        {time_24hr}
         {color} />
     </div>
   </div>
   <div class="columns">
     <div class="column">
       <label class="checkbox">
+        <input type="time" bind:value />
+        Value
+      </label>
+    </div>
+    <div class="column">
+      <label class="checkbox">
         <input type="checkbox" bind:checked={time_24hr} />
         Time Format 24
+      </label>
+    </div>
+    <div class="column">
+      <label class="checkbox">
+        <input type="checkbox" bind:checked={disabled} />
+        Disable
+      </label>
+    </div>
+    <div class="column">
+      <label class="checkbox">
+        <select bind:value={ui_color}>
+          <option value="primary">Primary</option>
+          <option value="danger">Danger</option>
+          <option value="success">Success</option>
+          <option value="warning">Warning</option>
+        </select>
       </label>
     </div>
   </div>
 </div>
 
 <script>
-  import { Panel, Timepicker } from "@kws3/ui";
+  import { Timepicker } from "@kws3/ui";
 
   export let value = "",
     style = "",
     placeholder = "Select Time..",
     disabled = false,
-    time_24hr = false,
+    time_24hr = true,
     options = {},
     ui_color = "primary",
     color = "";
-
-  let klass = "";
-  export { klass as class };
 </script>
