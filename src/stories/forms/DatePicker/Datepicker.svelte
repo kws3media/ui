8eafe0d88c27287b87c7c81c880dcd6eec861309
Sabir SM <sabirveli@gmail.com>
2022-07-27 19:21:53 +0530

    decorator fixes

--- src/stories/forms/DatePicker/Datepicker.svelte
+++ src/stories/forms/DatePicker/Datepicker.svelte
@@ -19,6 +19,12 @@
   <div class="columns">
     <div class="column">
       <label class="checkbox">
+        <input type="date" bind:value />
+        Value
+      </label>
+    </div>
+    <div class="column">
+      <label class="checkbox">
         <input type="checkbox" bind:checked={disabled} />
         Disable
       </label>
@@ -40,6 +46,51 @@
         </select>
       </label>
     </div>
+    <div class="column">
+      <label class="checkbox">
+        Min Date
+        <input type="date" bind:value={min_date} />
+      </label>
+      <label class="checkbox">
+        Max Date
+        <input type="date" bind:value={max_date} />
+      </label>
+    </div>
+
+    <div class="column">
+      <label class="checkbox">
+        <input type="checkbox" bind:checked={is_en_dts} />
+        Enabled Dates
+      </label>
+      {#if is_en_dts}
+        <label class="checkbox">
+          <input type="date" bind:value={en_dt} />
+          <button
+            type="button"
+            on:click={(event) => {
+              pushDate("en", event);
+            }}>Add</button>
+        </label>
+        <p style="font-size: 8px;">{enable_dates.toString()}</p>
+      {/if}
+
+      <label class="checkbox">
+        <input type="checkbox" bind:checked={is_ds_dts} />
+        Disabled Dates
+      </label>
+
+      {#if is_ds_dts}
+        <label class="checkbox">
+          <input type="date" bind:value={dis_dt} />
+          <button
+            type="button"
+            on:click={(event) => {
+              pushDate("ds", event);
+            }}>Add</button>
+        </label>
+        <p style="font-size: 8px;">{disable_dates.toString()}</p>
+      {/if}
+    </div>
   </div>
 </div>
 
@@ -50,11 +101,37 @@
     style = "",
     placeholder = "Select Date..",
     disabled = false,
-    min_date = null,
-    max_date = null,
+    min_date = "",
+    max_date = "",
     enable_dates = [],
     disable_dates = [],
     range_mode = false,
     options = {},
     calendar_color = "primary";
+
+  let en_dt = "";
+  let dis_dt = "";
+  let is_en_dts = false;
+  let is_ds_dts = false;
+
+  $: {
+    console.log(enable_dates);
+    if (is_en_dts || is_ds_dts) {
+      min_date = "";
+      max_date = "";
+    }
+  }
+
+  function pushDate(opt) {
+    if (opt === "en") {
+      let _newArr = enable_dates;
+      _newArr.push(en_dt.toString());
+      enable_dates = _newArr;
+    }
+    if (opt === "ds") {
+      let _newArrDs = disable_dates;
+      _newArrDs.push(dis_dt.toString());
+      disable_dates = _newArrDs;
+    }
+  }
 </script>
