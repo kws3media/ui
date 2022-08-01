1e969829ad155b5618ccc4db74ab6033b6b40d55
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-08-01 20:23:10 +0600

    story files

---
+++ src/stories/forms/SearchTool/searchtool.play.js
@@ -0,0 +1,18 @@
+import { expect } from "@storybook/jest";
+import {
+  within,
+  userEvent,
+  // eslint-disable-next-line no-unused-vars
+  waitFor,
+  // eslint-disable-next-line no-unused-vars
+  getNodeText,
+  fireEvent,
+} from "@storybook/testing-library";
+import { sleep } from "../../../../utils";
+
+// eslint-disable-next-line no-unused-vars
+export default async ({ args, canvasElement }) => {
+  // eslint-disable-next-line no-unused-vars
+  const canvas = within(canvasElement);
+  const inputs = canvasElement.querySelectorAll("input.input");
+};
