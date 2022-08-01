f3bf7846723df2519745303aac7d2af8218df83c
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-08-01 20:16:41 +0600

    fix

--- packages/@kws3/ui/forms/SearchTool.svelte
+++ packages/@kws3/ui/forms/SearchTool.svelte
@@ -18,38 +18,40 @@
 
 <script>
   import { Icon } from "@kws3/ui";
-  import { cloneObject, debounce } from "@kws3/utils";
+  import { debounce } from "@kws3/utils";
   import { onDestroy, onMount } from "svelte";
-  import { fuzzy, fuzzysearch } from "../../utils/fuzzysearch";
+  import { fuzzysearch } from "../utils/fuzzysearch";
 
   export let size = "small";
   export let placeholder = "Search";
   export let items = [];
   export let searchableKeys = [];
-  export let hasTextHighlighting = true;
+  export let highlighted_results = true;
   export let highlightColor = "danger-light";
   export let scoreThreshold = 5; //less value will give more results and vice versa
 
-  let keywords = "";
-  let orginalItems = [];
-  let isSearching = false;
+  let keywords = "",
+    orginalItems = [],
+    isSearching = false,
+    fuzzyOpts = {}; // fuzzy.js lib options
 
   const debouncedSearch = debounce(search, 300);
 
   // $: items, setItems(items, 1);
 
   onMount(() => {
-    if (fuzzy) {
-      fuzzy.analyzeSubTerms = true;
-      fuzzy.analyzeSubTermDepth = 10;
-      fuzzy.highlighting.before = "";
-      fuzzy.highlighting.after = "";
-      if (hasTextHighlighting) {
-        fuzzy.highlighting.before = `<span class="has-background-${highlightColor}">`;
-        fuzzy.highlighting.after = "</span>";
-      }
+    fuzzyOpts = {
+      analyzeSubTerms: true,
+      analyzeSubTermDepth: 10,
+      highlighting: {
+        after: "",
+        before: "",
+      },
+    };
+    if (highlighted_results) {
+      fuzzyOpts.highlighting.before = `<span class="h">`;
+      fuzzyOpts.highlighting.after = "</span>";
     }
-    console.log(items);
     orginalItems = [...items];
   });
 
