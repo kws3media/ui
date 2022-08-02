30ccb308e9aa45baa2455ed0c676dd2513796da2
Nurul Amin <namin2750@gmail.com>
2022-08-02 21:39:48 +0600

    change reviews format

--- src/stories/forms/SearchTool/data.js
+++ src/stories/forms/SearchTool/data.js
@@ -1,109 +1,279 @@
-export let good_reviews = [
+export let products_reviews = [
   {
     id: 1,
     review: "I bought this phone in Aug It is still perfect",
     complain: "Night mode does not work in front Camera",
   },
+  {
+    id: 2,
+    review: "The phone is very good, I am satisfied with it",
+    complain: "WhatsApp video call quality is not so good",
+  },
+  {
+    id: 3,
+    review: "Camera quality is great",
+    complain: "I have this phone and I don't like it",
+  },
 
-  "The phone is very good, I am satisfied with it",
-  "Camera quality is great",
-  "its camera is getting better day by day",
-  "Excellent phone in low price",
-  "Battery is good It lasts long time",
-  "Very fast charging phone, I loved it",
-  "I have been using this phone for 1 year, still so smooth and nice",
-  "It is still great, perfect and amazing battery life",
-  "Overall it's beautiful",
-  "Great gaming phone",
-  "The Phone is very good and fast, I am ok with it",
-  "In this price screen, battery and power is good",
-  "All is well but photo quality is very bad",
-  "Front camera is best that I have seen in recent times",
-  "This is very nice phone in reasonable price",
-  "The device has excellent battery life and impressive design",
-  "It is looking for the simplest Xiaomi phone",
-  "I like the phone Cheep, with good performance, great camera and stylish design",
-  "Very nice product and low costs",
-  "It has good quality for a reasonable price",
-  "I used different brands of phone, but Samsung is best brand",
-  "The keyboard performance is very good , compared its price",
-  "The wireless keyboard works great and was easy to set up and start using",
-  "The keyboard that I have been using has worked very well",
-  "it's very comfortable to use",
-  "RGB light looks so cool",
-  "This mouse is comfortable to use",
-  "I have been using it for 6 months, I suggest you to buy this keyboard",
-  "Best mouse ever",
-  "The mouse scrolling button works so smoothly",
-  "Battery quality is very good",
-  "This keyboard is so good, it's waterproof which is a good thing for me",
-  "The keyboard and mouse is beautiful",
-  "The keys feel great to use,I'm enjoying to use it",
-  "The mouse is so cute it changes colors every few seconds",
-  "It's very attractive if you like the stainless steel look",
-  "The mouse has a very nice feel",
-  "The computer is amazing, one of the best laptop I have used",
-  "Great computer, HP is well known for wonderful product",
-  "I am 5th year of owning my 450, never had an issue with it",
-  "I have been using HP laptop computers for over 20 years and love this brand",
-  "I am using external monitor with this laptop and it is a fantastic combination",
-  "HP has a good battery life I have never had a problem with it",
-  "The quality of their products are outstanding The equipment is normally an excellent price to purchase",
-  "This is the best keyboard I have ever use Very smooth for typing, perfect size, and quick response",
-  "Dell makes good quality laptop It's amazing, I loved it",
-  "My dell laptop is still in good condition",
-  "I have bought so many different brands of laptops over the years and Dell is clearly the best brand",
-  "The pricing of a lot of the items are really well in Dell",
-];
+  {
+    id: 4,
+    review: "its camera is getting better day by day",
+    complain: "Camera quality is very bad, compared its camera pixels",
+  },
 
-export let bad_reviews = [
-  "WhatsApp video call quality is not so good",
-  "I have this phone and I don't like it",
-  "Camera quality is very bad, compared its camera pixels",
-  "This phone has no face lock",
-  "Processor is very slow",
-  "Worst processor",
-  "Processor is as slow as a week in the jail",
-  "I bought this a month ago, it has hanging issue and  processor is very slow",
-  "Heavy weight, I'm not satisfied of this shit",
-  "Face and finger sensor is very bad",
-  "Camera quality is so poor",
-  "Doesn't support fast charging",
-  "It became slow after using a couple months",
-  "Screen is still really bright at lowest brightness setting",
-  "Display got yellow after using few months",
-  "The selfie camera is not good, what you get for 3mp camera",
-  "it's not perfect for plying game",
-  "It has heating issue",
-  "Worst phone ever it has worst camera",
-  "This one doesn't have a clear selfie, irritating, I hate this phone",
-  "The keyboard price is very high, compared its performance",
-  "The combo itself is nice looking, but it's cheaply made and you can feel it",
-  "it's not comfortable to use",
-  "RGB light does not work properly",
-  "This mouse is not comfortable to use",
-  "I suggest you not to buy this keyboard",
-  "Worst mouse ever",
-  "Its getting hot when using more than one hour",
-  "The mouse scrolling button broke within the first week of buying it",
-  "Sometime RGB doesn't light up",
-  "This laptop has heating problem after using more than 2- hours",
-  "Battery quality is very bad",
-  "I am using Asus laptop for  year, it has many issues",
-  "I suggest you not to buy this Asus laptop",
-  "I can't use external monitor in this laptop",
-  "I had faced many problem with it",
-  "This device has Wi-Fi issues, it gets disconnected on its own which is highly irritating,  when other divice is still connencted",
-  "It's one of the worst laptop from my side, I don't like the display and sounds quality",
-  "This is very worst mouse I have ever used",
-  "with dell I am having the worst experience from one month and still struggling",
-  "Never buy Dell if you want service They are terrible",
-  "Worst experience in my lif Bad product, worst costumer service",
-  "Dell used to be a great bran Sadly the brand has no longer the quality",
-  "Dell Technologies do not support their products very well",
-  "I bought an Asus monitor, It's a worst monitor",
-  "Asus laptop sound is not working good, volume is not high",
-  "A brand new laptop came with issues and it was not able to boot up, so it,s a worst brand",
-  "Acer is joke brand, I don't recommend Acer brand",
-  "Never buy an Acer laptop, my device stopped working after 7 months",
+  {
+    id: 5,
+    review: "Excellent phone in low price",
+    complain: "This phone has no face lock",
+  },
+
+  {
+    id: 6,
+    review: "Battery is good It lasts long time",
+    complain: "Processor is very slow",
+  },
+  {
+    id: 7,
+    review: "Very fast charging phone, I loved it",
+    complain: "Worst processor",
+  },
+  {
+    id: 8,
+    review: "I have been using this phone for 1 year, still so smooth and nice",
+    complain: "Processor is as slow as a week in the jail",
+  },
+  {
+    id: 9,
+    review: "It is still great, perfect and amazing battery life",
+    complain:
+      "I bought this a month ago, it has hanging issue and  processor is very slow",
+  },
+  {
+    id: 10,
+    review: "Great gaming phone",
+    complain: "Heavy weight, I'm not satisfied of this shit",
+  },
+  {
+    id: 11,
+    review: "The Phone is very good and fast, I am ok with it",
+    complain: "Face and finger sensor is very bad",
+  },
+  {
+    id: 12,
+    review: "In this price screen, battery and power is good",
+    complain: "Camera quality is so poor",
+  },
+  {
+    id: 13,
+    review: "All is well but photo quality is very bad",
+    complain: "Doesn't support fast charging",
+  },
+  {
+    id: 14,
+    review: "Front camera is best that I have seen in recent times",
+    complain: "It became slow after using a couple months",
+  },
+  {
+    id: 15,
+    review: "This is very nice phone in reasonable price",
+    complain:
+      "Never buy an Acer laptop, my device stopped working after 7 months",
+  },
+  {
+    id: 16,
+    review: "The device has excellent battery life and impressive design",
+    complain: "Screen is still really bright at lowest brightness setting",
+  },
+  {
+    id: 17,
+    review: "It is looking for the simplest Xiaomi phone",
+    complain: "Display got yellow after using few months",
+  },
+  {
+    id: 18,
+    review:
+      "I like the phone Cheep, with good performance, great camera and stylish design",
+    complain: "The selfie camera is not good, what you get for 3mp camera",
+  },
+  {
+    id: 19,
+    review: "Very nice product and low costs",
+    complain: "it's not perfect for plying game",
+  },
+  {
+    id: 20,
+    review: "It has good quality for a reasonable price",
+    complain: "It has heating issue",
+  },
+  {
+    id: 21,
+    review: "I used different brands of phone, but Samsung is best brand",
+    complain: "Worst phone ever it has worst camera",
+  },
+  {
+    id: 22,
+    review: "The keyboard performance is very good , compared its price",
+    complain:
+      "A brand new laptop came with issues and it was not able to boot up, so it,s a worst brand",
+  },
+  {
+    id: 23,
+    review:
+      "The wireless keyboard works great and was easy to set up and start using",
+    complain:
+      "This one doesn't have a clear selfie, irritating, I hate this phone",
+  },
+  {
+    id: 24,
+    review:
+      "The wireless keyboard works great and was easy to set up and start using",
+    complain:
+      "This one doesn't have a clear selfie, irritating, I hate this phone",
+  },
+  {
+    id: 25,
+    review:
+      "The wireless keyboard works great and was easy to set up and start using",
+    complain:
+      "The combo itself is nice looking, but it's cheaply made and you can feel it",
+  },
+  {
+    id: 26,
+    review:
+      "The wireless keyboard works great and was easy to set up and start using",
+    complain: "it's not comfortable to use",
+  },
+  {
+    id: 27,
+    review:
+      "The wireless keyboard works great and was easy to set up and start using",
+    complain: "This mouse is not comfortable to use",
+  },
+  {
+    id: 28,
+    review: "RGB light looks so cool",
+    complain: "I suggest you not to buy this keyboard",
+  },
+  {
+    id: 29,
+    review: "This mouse is comfortable to use",
+    complain: "Worst mouse ever",
+  },
+  {
+    id: 30,
+    review:
+      "I have been using it for 6 months, I suggest you to buy this keyboard",
+    complain: "I am using Asus laptop for  year, it has many issues",
+  },
+  {
+    id: 31,
+    review: "Best mouse ever",
+    complain: "Its getting hot when using more than one hour",
+  },
+  {
+    id: 32,
+    review: "The mouse scrolling button works so smoothly",
+    complain:
+      "The mouse scrolling button broke within the first week of buying it",
+  },
+  {
+    id: 33,
+    review:
+      "This keyboard is so good, it's waterproof which is a good thing for me",
+    complain: "Sometime RGB doesn't light up",
+  },
+  {
+    id: 34,
+    review: "The keyboard and mouse is beautiful",
+    complain: "This laptop has heating problem after using more than 2- hours",
+  },
+  {
+    id: 35,
+    review: "The keys feel great to use,I'm enjoying to use it",
+    complain: "Battery quality is very bad",
+  },
+  {
+    id: 36,
+    review: "The mouse is so cute it changes colors every few seconds",
+    complain: "I suggest you not to buy this Asus laptop",
+  },
+  {
+    id: 37,
+    review: "It's very attractive if you like the stainless steel look",
+    complain: "I can't use external monitor in this laptop",
+  },
+  {
+    id: 38,
+    review: "The mouse has a very nice feel",
+    complain: "I had faced many problem with it",
+  },
+  {
+    id: 39,
+    review: "The computer is amazing, one of the best laptop I have used",
+    complain:
+      "This device has Wi-Fi issues, it gets disconnected on its own which is highly irritating,  when other divice is still connencted",
+  },
+  {
+    id: 40,
+    review: "Great computer, HP is well known for wonderful product",
+    complain:
+      "It's one of the worst laptop from my side, I don't like the display and sounds quality",
+  },
+  {
+    id: 41,
+    review: "I am 5th year of owning my 450, never had an issue with it",
+    complain: "This is very worst mouse I have ever used",
+  },
+  {
+    id: 42,
+    review:
+      "I have been using HP laptop computers for over 20 years and love this brand",
+    complain:
+      "with dell I am having the worst experience from one month and still struggling",
+  },
+  {
+    id: 43,
+    review:
+      "I am using external monitor with this laptop and it is a fantastic combination",
+    complain: "Never buy Dell if you want service They are terrible",
+  },
+  {
+    id: 44,
+    review: "HP has a good battery life I have never had a problem with it",
+    complain: "Worst experience in my lif Bad product, worst costumer service",
+  },
+  {
+    id: 45,
+    review:
+      "The quality of their products are outstanding The equipment is normally an excellent price to purchase",
+    complain:
+      "Dell used to be a great bran Sadly the brand has no longer the quality",
+  },
+  {
+    id: 46,
+    review:
+      "This is the best keyboard I have ever use Very smooth for typing, perfect size, and quick response",
+    complain: "WhatsApp video call quality is not so good",
+  },
+  {
+    id: 47,
+    review: "Dell makes good quality laptop It's amazing, I loved it",
+    complain: "Dell Technologies do not support their products very well",
+  },
+  {
+    id: 48,
+    review: "My dell laptop is still in good condition",
+    complain: "I bought an Asus monitor, It's a worst monitor",
+  },
+  {
+    id: 49,
+    review: "The pricing of a lot of the items are really well in Dell",
+    complain: "Asus laptop sound is not working good, volume is not high",
+  },
+  {
+    id: 50,
+    review:
+      "I have bought so many different brands of laptops over the years and Dell is clearly the best brand",
+    complain: "Acer is joke brand, I don't recommend Acer brand",
+  },
 ];
