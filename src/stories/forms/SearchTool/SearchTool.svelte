f71b38da62db709aa9471939844b2ede6fd394fc
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-08-02 22:24:17 +0600

    visual fix

--- src/stories/forms/SearchTool/SearchTool.svelte
+++ src/stories/forms/SearchTool/SearchTool.svelte
@@ -1,6 +1,6 @@
-<div id="ss_container">
-  <div class="columns">
-    <div class="column is-6">
+<div class="columns">
+  <div class="column">
+    {#if mounted}
       <KwsSearchTool
         {size}
         {color}
@@ -13,28 +13,62 @@
         class={klass}
         {disabled}
         {readonly} />
-      Output data:
-      <code>{JSON.stringify(selected_character, null, 2)}</code>
-    </div>
+    {/if}
+  </div>
+</div>
+<div class="columns">
+  <div class="column">
+    <h3 class="title is-4">Reviews</h3>
+    {#each options as { review }}
+      <Message
+        color="primary"
+        class="is-marginless mb-2"
+        inner_class="is-paddingless px-2 py-1">{@html review}</Message>
+    {/each}
+  </div>
+  <div class="column">
+    <h3 class="title is-4">Complains</h3>
+    {#each options as { complain }}
+      <Message
+        color="warning"
+        class="is-marginless mb-2"
+        inner_class="is-paddingless px-2 py-1">{@html complain}</Message>
+    {/each}
   </div>
 </div>
 
+<style>
+  span.h {
+    font-weight: 700;
+    color: currentColor;
+    background: transparent;
+  }
+</style>
+
 <script>
-  import { SearchTool as KwsSearchTool } from "@kws3/ui";
+  import { products_reviews } from "./data.js";
+  import { SearchTool as KwsSearchTool, Message } from "@kws3/ui";
 
   import { onMount } from "svelte";
+
+  onMount(() => {
+    options = products_reviews;
+    mounted = true;
+  });
 
   export let size = "",
     color = "",
     style = "",
     disabled = false,
     readonly = false,
+    mounted = false,
     options = [],
-    searchableKeys = [],
-    search_strategy = "fuzzy",
+    searchableKeys = ["review", "complain"],
     highlighted_results = true,
     scoreThreshold = 5,
     placeholder = "Marvel characters...";
+
+  console.log(options);
 
   let klass = "";
   export { klass as class };
