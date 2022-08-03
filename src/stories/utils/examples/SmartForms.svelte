b8b740bada979cd1b9a10d3199bfd7c8866f871b
nikeetaa231 <nikeetaa231@gmail.com>
2022-08-03 19:57:28 +0530

    smart form component

---
+++ src/stories/utils/examples/SmartForms.svelte
@@ -0,0 +1,76 @@
+<div id="tooltip-example-container-1">
+  <div class="columns is-multiline">
+    <div class="column">
+      <form on:submit|preventDefault|stopPropagation={save}>
+        <div class="field">
+          <div class="control">
+            <label for="" class="label">Email</label>
+            <input
+              class="input {$errors.email ? 'is-danger' : ''}"
+              type="text"
+              bind:value={$data.email} />
+          </div>
+        </div>
+
+        <div class="field">
+          <p class="control">
+            <label for="" class="label">Password</label>
+            <input
+              class="input {$errors.password ? 'is-danger' : ''}"
+              type="password"
+              bind:value={$data.password} />
+          </p>
+        </div>
+
+        <div class="field">
+          <p class="control">
+            <button
+              type="submit"
+              class="button is-primary is-pulled-right"
+              disabled={!$isValid}>Login</button>
+          </p>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script>
+  import { makeForm } from "@kws3/ui";
+
+  const notEmpty = (v) => v && v.trim() !== "";
+  const withMsg = (msg, fn) => (v, otherFields) =>
+    fn(v, otherFields) ? "" : msg;
+
+  let form;
+  let data;
+  let errors;
+  let isValid;
+  let isTouched;
+  let validators = {
+    email: withMsg("Email should not be empty", notEmpty),
+    password: withMsg("Password should not be empty", notEmpty),
+  };
+
+  $: load();
+
+  function load() {
+    form = makeForm({
+      data: {
+        email: "",
+        password: "",
+      },
+      validators,
+    });
+    data = form.formData;
+    errors = form.errors;
+    isValid = form.isValid;
+    isTouched = form.isTouched;
+  }
+
+  function save() {
+    if (!isTouched) {
+      form.update(data);
+    }
+  }
+</script>
