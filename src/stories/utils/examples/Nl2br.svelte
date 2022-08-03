3d971f885161f10e45767d40a6fddfd998661fa0
Sabir SM <sabirveli@gmail.com>
2022-08-03 15:03:53 +0530

    clone object function

--- src/stories/utils/examples/Nl2br.svelte
+++ src/stories/utils/examples/Nl2br.svelte
@@ -8,7 +8,7 @@
       <h2 class="subtitle is-5">Text after html break</h2>
       <div>
         <!-- eslint-disable-next-line @ota-meshi/svelte/no-at-html-tags -->
-        {@html new_text}
+        {@html nl_2_br(text)}
       </div>
     </div>
   </div>
@@ -16,11 +16,5 @@
 
 <script>
   import { nl_2_br } from "@kws3/ui";
-
   let text = `Lorem ipsum dolor sit amet\n, consectetur adipiscing elit\n, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`;
-  let new_text = "";
-  $: {
-    new_text = nl_2_br(text);
-    console.log(new_text);
-  }
 </script>
