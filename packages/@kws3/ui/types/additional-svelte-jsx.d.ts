cf4c0eb919cd61d0b6e8a76337655d4f14a8aca8
Sabir SM <sabirveli@gmail.com>
2022-08-17 18:21:13 +0530

    custom events for date/time pickers

--- packages/@kws3/ui/types/additional-svelte-jsx.d.ts
+++ packages/@kws3/ui/types/additional-svelte-jsx.d.ts
@@ -5,5 +5,12 @@ declare namespace svelte.JSX {
     onhiding?: (event: any) => any;
     onhidden?: (event: any) => any;
     ontriggered?: (event: any) => any;
+    onclose?: (event: any) => any;
+    ontimeChange?: (event: any) => any;
+    onready?: (event: any) => any;
+    onopen?: (event: any) => any;
+    ondateChange?: (event: any) => any;
+    onmonthChange?: (event: any) => any;
+    onyearChange?: (event: any) => any;
   }
 }
