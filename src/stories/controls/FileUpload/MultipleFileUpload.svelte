aa56cdfd2ec27f27fe17ca8483d42a135e4d4e16
nikeetaa231 <nikeetaa231@gmail.com>
2022-08-31 16:05:56 +0530

    slot design fixes for multiple files upload

--- src/stories/controls/FileUpload/MultipleFileUpload.svelte
+++ src/stories/controls/FileUpload/MultipleFileUpload.svelte
@@ -4,7 +4,6 @@
       on:file_chosen={(event) => onFileChosen(event, false)}
       on:file_uploaded={() => onFileUploaded()}
       on:file_upload_error={() => onFileUploadError()}
-      {multiple}
       {allowed}
       {disabled}
       {message}
@@ -14,7 +13,36 @@
       class={klass}
       {color}
       {size}
-      {info_color} />
+      {info_color}
+      {multiple}
+      {accept}
+      inner_style="min-height: 2.5rem;">
+      <div slot="default" let:filename let:uploading let:progress let:finished>
+        <div class="up-icon">
+          {#if uploading}
+            <span class="loader" />
+          {:else if finished}
+            <Icon size="" icon="check-circle" class="fa-lg" />
+          {:else}
+            <Icon size="" icon="upload" class="fa-lg" />
+          {/if}
+        </div>
+        <div class="file">
+          {#if uploading}
+            <div class="upload-progress">
+              <div class="progress-inner">
+                <div class="bar" style="width:{progress}%" />
+              </div>
+            </div>
+            <div class="progress-caption">{progress}% - Uploading...</div>
+          {:else if finished}
+            <div class="filename">Upload complete!</div>
+          {:else}
+            <div class="filename"><span>{filename}</span></div>
+          {/if}
+        </div>
+      </div>
+    </KwsFileUpload>
     <p class="is-block mt-2 has-text-success">This will succeed.</p>
   </div>
 
@@ -23,7 +51,6 @@
       on:file_chosen={(event) => onFileChosen(event, true)}
       on:file_uploaded={() => onFileUploaded()}
       on:file_upload_error={() => onFileUploadError()}
-      {multiple}
       {allowed}
       {disabled}
       {message}
@@ -33,11 +60,90 @@
       class={klass}
       {color}
       {size}
-      {info_color} />
+      {info_color}
+      {multiple}
+      {accept}
+      inner_style="min-height:10rem">
+      <div
+        class="file has-name is-boxed"
+        slot="default"
+        let:filename
+        let:uploading
+        let:progress
+        let:finished
+        let:error
+        let:fileTypes
+        let:maxFileSize
+        let:error_message>
+        <label class="file-label" for="">
+          <div class="file-cta">
+            <div>
+              <span>
+                {#if uploading}
+                  <span class="loader" style="display:inline-block" />
+                {:else if finished}
+                  <Icon size="small" icon="check-circle" />
+                {:else}
+                  <Icon size="small" icon="plus" />
+                {/if}
+              </span>
+              {#if uploading}
+                <span class="progress-caption has-text-centered">
+                  {progress ? progress + "% - " : ""} Uploading...
+                </span>
+              {:else if finished}
+                <span class="filename">Upload complete!</span>
+              {:else}
+                <span class="filename"><span>{filename}</span></span>
+              {/if}
+            </div>
+            {#if uploading}
+              <div class="upload-progress">
+                <div class="progress-inner">
+                  <div class="bar" style="width:{progress}%;" />
+                </div>
+              </div>
+            {/if}
+          </div>
+          <span class="file-name">
+            <div class="level mt-2">
+              {#if error}
+                <div
+                  class="level-item"
+                  style="max-width:100%;white-space: break-spaces;">
+                  <span class="help is-{error ? 'danger' : ''}"
+                    >{error_message}</span>
+                </div>
+              {:else}
+                <div class="level-item">
+                  <span class="help">
+                    {!multiple ? "Max size:" + maxFileSize : ""}
+                  </span>
+                </div>
+              {/if}
+            </div>
+
+            {#if !error}
+              <div class="control" style="max-width:100%">
+                <span class="help has-text-centered">{fileTypes}</span>
+              </div>
+            {/if}
+
+            {#if info}
+              <div class="control" style="max-width:100%">
+                <span class="help has-text-{info_color}">{info}</span>
+              </div>
+            {/if}
+          </span>
+          <div class="drop-on-me">Drop files here to upload</div>
+        </label>
+      </div>
+    </KwsFileUpload>
     <p class="is-block mt-2 has-text-danger">This will fail.</p>
   </div>
 </div>
 
+{images.length}
 {#if images && images.length}
   <div class="columns">
     <div class="column">
@@ -54,12 +160,12 @@
 
 <script>
   import { createEventDispatcher } from "svelte";
-  import { FileUpload as KwsFileUpload } from "@kws3/ui";
+  import { FileUpload as KwsFileUpload, Icon } from "@kws3/ui";
 
   const fire = createEventDispatcher();
 
   export let key = "",
-    message = "Choose File...",
+    message = "Choose Files...",
     info = "",
     max = 5000000,
     allowed = "*",
@@ -80,7 +186,8 @@
      * Whether to upload multiple or single file(s).
      * @type {boolean}
      */
-    multiple = true;
+    multiple = true,
+    accept = "*";
 
   let klass = "";
   export { klass as class };
