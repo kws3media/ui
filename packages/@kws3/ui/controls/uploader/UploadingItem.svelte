3df5d5a49144b4b4cbfc73b26cbec4e6d8ebd69e
Sabir SM <sabirveli@gmail.com>
2022-09-14 18:19:38 +0530

    upload notification helper comps

---
+++ packages/@kws3/ui/controls/uploader/UploadingItem.svelte
@@ -0,0 +1,15 @@
+<div class="control is-expanded">
+  <span
+    class="is-block filename {item.status === 'failed'
+      ? 'has-text-danger'
+      : ''}">{item.original_name}</span>
+  <progress
+    class="info is-small"
+    style="width:100%"
+    max="100"
+    value={item.progress || 0} />
+</div>
+
+<script>
+  export let item = {};
+</script>
