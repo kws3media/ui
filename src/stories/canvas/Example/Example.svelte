8cdf0bb14098de57014394bf66ff621dbf062ef6
Saiful <msisaifulsaif@gmail.com>
2023-03-27 22:43:23 +0600

    Add an esignature example

---
+++ src/stories/canvas/Example/Example.svelte
@@ -0,0 +1,67 @@
+<div class="columns">
+  <div class="column has-text-centered">
+    <label for="" class="label">Input</label>
+    <KwsCanvas
+      bind:this={canvasWrapper}
+      bind:image
+      {styles}
+      {width}
+      {height}
+      {pen_width}
+      {eraser_width}
+      {pen_color}
+      {background_image}
+      {readonly}
+      {disabled}
+      {initial_scale}
+      {expand}
+      {tools}
+      {active_tool}
+      {actions}
+      {toolbar_placement}
+      {drawing_label}
+      {cy} />
+  </div>
+  <div class="column has-text-centered">
+    <label for="" class="label">Output</label>
+    {#if image}
+      <img
+        alt="canvasoutput"
+        src={image}
+        {width}
+        {height}
+        style="border: 1px solid #b5b5b5;" />
+    {/if}
+  </div>
+</div>
+
+<script>
+  import { Canvas as KwsCanvas } from "@kws3/ui";
+
+  let canvasWrapper;
+
+  export let styles = {
+      border: "1px solid #b5b5b5",
+    },
+    width = "250px",
+    height = "250px",
+    pen_width = 2,
+    eraser_width = 6,
+    pen_color = "#ff0000",
+    background_image = "",
+    readonly = false,
+    disabled = false,
+    image = "",
+    initial_scale = 1,
+    expand = {
+      from: "center",
+      to: "center",
+      scale: 50,
+    },
+    tools = ["Pen", "Eraser"],
+    actions = ["controls", "colorpicker", "undo", "redo", "reset", "expand"],
+    toolbar_placement = "bottom",
+    active_tool = "Pen",
+    drawing_label = "",
+    cy = "";
+</script>
