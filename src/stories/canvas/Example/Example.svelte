eeed07a57631ed6a5bdd322378ce3f69defdb77f
Sharif Ahmed <me.sharifahmed@gmail.com>
2023-03-28 20:26:26 +0600

    fix

--- src/stories/canvas/Example/Example.svelte
+++ src/stories/canvas/Example/Example.svelte
@@ -1,26 +1,17 @@
 <div class="columns">
   <div class="column has-text-centered">
-    <label for="" class="label">Input</label>
+    <label for="" class="label">Please sign here</label>
     <KwsCanvas
       bind:image
       bind:is_filled
       {styles}
       {width}
       {height}
-      {line}
-      {eraser}
-      {color}
       {background_image}
       {readonly}
       {disabled}
-      {initial_scale}
-      {expand}
-      {tools}
       {activeTool}
       {actions}
-      {toolbar_placement}
-      {drawing_label}
-      {cy}
       {has_controls}
       {control_position} />
   </div>
@@ -46,24 +37,11 @@
     },
     width = "520px",
     height = "170px",
-    line = 2,
-    eraser = 6,
-    color = "#000000",
     background_image = "",
     readonly = false,
     disabled = false,
-    initial_scale = 1,
-    expand = {
-      from: "center",
-      to: "center",
-      scale: 50,
-    },
-    tools = ["Pen", "Eraser"],
     actions = ["undo", "redo", "reset"],
-    toolbar_placement = "bottom",
     activeTool = "Pen",
-    drawing_label = "",
-    cy = "",
     has_controls = true,
     control_position = "end";
 </script>
