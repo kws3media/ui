4fac6140e90d3b09da7d5a18bacbb3ce03b8b1a6
Sharif Ahmed <me.sharifahmed@gmail.com>
2023-04-04 16:38:56 +0600

    more examples fix

--- src/stories/canvas/Example/Example.svelte
+++ src/stories/canvas/Example/Example.svelte
@@ -1,30 +1,11 @@
 <div class="columns mb-6">
   <div class="column has-text-centered">
     <label for="" class="label">Input</label>
-    <KwsESignature
+    <KwsCanvas
       bind:image
-      {styles}
       {width}
-      {height}
-      {pen_width}
-      {eraser_width}
-      {pen_color}
       {background_image}
-      {background_size}
-      {background_repeat}
-      {readonly}
-      {disabled}
-      {initial_scale}
-      {expand_to}
-      {expand_from}
-      {expand_scale}
-
-      {tools}
-      {active_tool}
-      {actions}
-      {toolbar_placement}
-      {drawing_label}
-      {cy} />
+     />
   </div>
   <div class="column has-text-centered">
     <label for="" class="label">Output</label>
@@ -41,33 +22,21 @@
   </div>
 </div>
 
+
+<style lang='scss'>
+  :global(.kws-canvas-wrapper .kws-pen-input canvas){
+      background-repeat: repeat;
+      background-size: cover;
+    }
+</style>
+
 <script>
-  import { ESignature as KwsESignature} from "@kws3/ui";
+  import { Canvas as KwsCanvas} from "@kws3/ui";
 
-  export let image, is_filled;
+  export let image;
 
 
-  export let styles = {
-      border: "1px solid #b5b5b5",
-    },
-    width = "250px",
-    height = "250px",
-    pen_width = 2,
-    eraser_width = 6,
-    pen_color = "#ff0000",
-    background_image = "",
-    background_size = "cover",
-    background_repeat = "no-repeat",
-    readonly = false,
-    disabled = false,
-    initial_scale = 1,
-    expand_to = "center",
-    expand_from = "center",
-    expand_scale = 50,
-    tools = ["Pen", "Eraser"],
-    actions = ["controls", "colorpicker", "undo", "redo", "reset", "expand"],
-    toolbar_placement = "bottom",
-    active_tool = "Pen",
-    drawing_label = "",
-    cy = "";
+  export let width = "350px",
+    height='250px',
+    background_image = "/images/canvas_bg.png";
 </script>
