bf29c6e447ade2adb012e36423dd2c34742537d8
Sharif Ahmed <me.sharifahmed@gmail.com>
2023-04-20 20:22:10 +0600

    fixes

--- packages/@kws3/sveltekit-preprocess/src/script/importsMapGen.js
+++ packages/@kws3/sveltekit-preprocess/src/script/importsMapGen.js
@@ -2,14 +2,48 @@ const fs = require("fs");
 
 // Define the list of exports
 const exportsList = `
+export { applySettings } from "./settings.js";
+
+export { activateTooltips, tooltip, popover } from "./helpers/Tooltip.js";
+export { default as Popover } from "./helpers/Popover.svelte";
+export { default as Icon } from "./helpers/Icon.svelte";
+export { default as Message } from "./helpers/Message.svelte";
+export { default as Modal } from "./helpers/Modal.svelte";
+export { default as CardModal } from "./helpers/CardModal.svelte";
+export { default as Panel } from "./helpers/Panel.svelte";
+export { default as Notification } from "./helpers/Notification.svelte";
+export { default as Loader } from "./helpers/Loader.svelte";
+export { default as ActionSheet } from "./helpers/ActionSheet.svelte";
+export { default as Skeleton } from "./helpers/Skeleton.svelte";
+export { default as Divider } from "./helpers/Divider.svelte";
+export { default as Timeline } from "./helpers/Timeline/Timeline.svelte";
+export { default as TimelineItem } from "./helpers/Timeline/TimelineItem.svelte";
+export { default as TimelineHeader } from "./helpers/Timeline/TimelineHeader.svelte";
+export { default as Nl2br } from "./helpers/Nl2br.svelte";
+export { default as ClipboardCopier } from "./helpers/ClipboardCopier.svelte";
+export { default as ScrollableList } from "./helpers/ScrollableList.svelte";
+export {
+  alert,
+  confirm,
+  prompt,
+  default as Dialog,
+} from "./helpers/Dialog/index.js";
+export {
+  Notifications,
+  Toasts,
+  Snackbars,
+  FloatiesStore,
+} from "./helpers/FloatingUI/index.js";
 export { default as FloatingUIOutput } from "./helpers/FloatingUI/FloatingUIOutput.svelte";
 export { default as Floatie } from "./helpers/FloatingUI/Floatie.svelte";
 export { portal } from "svelte-portal";
 export { default as Portal } from "svelte-portal";
+
 export { default as ConfirmButton } from "./buttons/ConfirmButton.svelte";
 export { default as DeleteButton } from "./buttons/DeleteButton.svelte";
 export { default as SubmitButton } from "./buttons/SubmitButton.svelte";
 export { default as ProcessButton } from "./buttons/ProcessButton.svelte";
+
 export { default as Checkbox } from "./controls/Checkbox.svelte";
 export { default as FileUpload } from "./controls/FileUpload.svelte";
 export { default as NumberInput } from "./controls/NumberInput.svelte";
@@ -18,11 +52,46 @@ export { default as Toggle } from "./controls/Toggle.svelte";
 export { default as ToggleButtons } from "./controls/ToggleButtons.svelte";
 export { default as ToggleControl } from "./controls/ToggleControl.svelte";
 export { default as RangeSlider } from "./controls/RangeSlider.svelte";
+
 export { default as Canvas } from "./canvas/Canvas.svelte";
 export { default as ESignature } from "./canvas/ESignature.svelte";
 export { default as PenControls } from "./canvas/PenControls.svelte";
 export { default as PenInput } from "./canvas/PenInput.svelte";
 export { Pen, Eraser } from "./internal/DrawingPad.js";
+
+export { default as Transition } from "./transitions/Transition.svelte";
+
+export { default as SlidingPane } from "./sliding-panes/SlidingPane.svelte";
+export { default as SlidingPaneSet } from "./sliding-panes/SlidingPaneSet.svelte";
+
+export { default as AutoComplete } from "./forms/AutoComplete.svelte";
+export { default as SearchableSelect } from "./forms/select/SearchableSelect.svelte";
+export { default as MultiSelect } from "./forms/select/MultiSelect.svelte";
+export { default as MaskedInput } from "./forms/MaskedInput.svelte";
+export { default as PasswordInput } from "./forms/PasswordInput.svelte";
+export { default as Colorpicker } from "./forms/colorpicker/Colorpicker.svelte";
+export { default as Datepicker } from "./forms/Datepicker.svelte";
+export { default as Timepicker } from "./forms/Timepicker.svelte";
+export { default as SearchInput } from "./forms/SearchInput.svelte";
+export { datepicker as DatepickerAction } from "./forms/actions.js";
+export { timepicker as TimepickerAction } from "./forms/actions.js";
+export { default as PasswordValidator } from "./forms/PasswordValidator/PasswordValidator.svelte";
+
+export { default as GridView } from "./datagrid/GridView/GridView.svelte";
+export { default as TileView } from "./datagrid/TileView/TileView.svelte";
+export { default as DataSearch } from "./datagrid/DataSearch/DataSearch.svelte";
+export { default as Pagination } from "./datagrid/Pagination/Pagination.svelte";
+export { default as DataSort } from "./datagrid/DataSort/DataSort.svelte";
+
+export { default as Chart } from "./charts/Chart.svelte";
+export { default as DonutChart } from "./charts/DonutChart.svelte";
+export { default as PieChart } from "./charts/PieChart.svelte";
+export { default as MixedChart } from "./charts/MixedChart.svelte";
+export { default as BarChart } from "./charts/BarChart.svelte";
+export { default as LineChart } from "./charts/LineChart.svelte";
+export { default as AreaChart } from "./charts/AreaChart.svelte";
+export { default as RadialChart } from "./charts/RadialChart.svelte";
+
 `;
 
 // Parse the exports into an array of objects
@@ -42,7 +111,9 @@ expls.forEach((line) => {
     // console.log(match)
     exportNames.forEach((name) => {
       let _name = name.trim();
+      let _filePath = filePath.trim();
       let type = "";
+      let packageType = "";
 
       if (_name.startsWith("default")) {
         _name = _name.replace("default as", "");
@@ -50,17 +121,25 @@ expls.forEach((line) => {
       } else {
         type = "named";
       }
+
+      if (_filePath.startsWith("./")) {
+        _filePath = _filePath.replace("./", "");
+        packageType = "internal";
+      } else {
+        packageType = "external";
+      }
       exportsArray.push({
         name: _name.trim(),
         exportType: type,
-        filepath: filePath,
+        filepath: _filePath,
+        packageType,
       });
     });
   }
 });
 
 // Write the exports array to a JSON file
-// fs.writeFileSync('exports.json', JSON.stringify(exportsArray, null, 2));
+fs.writeFileSync("exports.json", JSON.stringify(exportsArray, null, 2));
 console.log(exportsArray);
 
 console.log("JSON file generated successfully!");
