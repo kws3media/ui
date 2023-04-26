d75bdcafd0cbbe351c74b965952fdf1dcd9e4d42
nikeetaa231 <nikeetaa231@gmail.com>
2023-04-26 18:37:44 +0530

    helpers for tsx file generation

---
+++ scripts/svelte2tsx/helpers/filesystem.js
@@ -0,0 +1,119 @@
+var fs = require("fs");
+var path = require("path");
+
+/** @param {string} dir */
+function mkdirp(dir) {
+  try {
+    fs.mkdirSync(dir, { recursive: true });
+  } catch (/** @type {any} */ e) {
+    if (e.code === "EEXIST") return;
+    throw e;
+  }
+}
+
+/** @param {string} path */
+function rimraf(path) {
+  fs.rmSync(path, { force: true, recursive: true });
+}
+
+/** @param {string} str */
+function posixify(str) {
+  return str.replace(/\\/g, "/");
+}
+
+/**
+ * Get a list of all files in a directory
+ * @param {string} cwd - the directory to walk
+ * @param {boolean} [dirs] - whether to include directories in the result
+ */
+function walk(cwd, dirs = false) {
+  /** @type {string[]} */
+  const all_files = [];
+
+  /** @param {string} dir */
+  function walk_dir(dir) {
+    const files = fs.readdirSync(path.join(cwd, dir));
+
+    for (const file of files) {
+      const joined = path.join(dir, file);
+      const stats = fs.statSync(path.join(cwd, joined));
+      if (stats.isDirectory()) {
+        if (dirs) all_files.push(joined);
+        walk_dir(joined);
+      } else {
+        all_files.push(joined);
+      }
+    }
+  }
+
+  return walk_dir(""), all_files;
+}
+
+/**
+ * @param {string} source
+ * @param {string} target
+ * @param {{
+ *   filter?: (basename: string) => boolean;
+ *   replace?: Record<string, string>;
+ * }} opts
+ */
+function copy(source, target, opts = {}) {
+  if (!fs.existsSync(source)) return [];
+
+  /** @type {string[]} */
+  const files = [];
+
+  const prefix = posixify(target) + "/";
+
+  const regex = opts.replace
+    ? new RegExp(`\\b(${Object.keys(opts.replace).join("|")})\\b`, "g")
+    : null;
+
+  /**
+   * @param {string} from
+   * @param {string} to
+   */
+  function go(from, to) {
+    if (opts.filter && !opts.filter(path.basename(from))) return;
+
+    const stats = fs.statSync(from);
+
+    if (stats.isDirectory()) {
+      fs.readdirSync(from).forEach((file) => {
+        go(path.join(from, file), path.join(to, file));
+      });
+    } else {
+      mkdirp(path.dirname(to));
+
+      if (opts.replace) {
+        const data = fs.readFileSync(from, "utf-8");
+        fs.writeFileSync(
+          to,
+          data.replace(
+            /** @type {RegExp} */ (regex),
+            (_match, key) =>
+              /** @type {Record<string, string>} */ (opts.replace)[key]
+          )
+        );
+      } else {
+        fs.copyFileSync(from, to);
+      }
+
+      files.push(
+        to === target
+          ? posixify(path.basename(to))
+          : posixify(to).replace(prefix, "")
+      );
+    }
+  }
+
+  go(source, target);
+
+  return files;
+}
+
+exports.mkdirp = mkdirp;
+exports.rimraf = rimraf;
+exports.posixify = posixify;
+exports.walk = walk;
+exports.copy = copy;
---
+++ scripts/svelte2tsx/helpers/utils.js
@@ -0,0 +1,112 @@
+var fs = require("fs");
+var path = require("path");
+var { posixify, mkdirp, walk } = require("./filesystem.js");
+
+/**
+ * Resolves aliases
+ *
+ * @param {string} input
+ * @param {string} file Relative to the input
+ * @param {string} content
+ * @param {Record<string, string>} aliases
+ * @returns {string}
+ */
+function resolve_aliases(input, file, content, aliases) {
+  /**
+   * @param {string} match
+   * @param {string} _
+   * @param {string} import_path
+   */
+  const replace_import_path = (match, _, import_path) => {
+    for (const [alias, value] of Object.entries(aliases)) {
+      if (!import_path.startsWith(alias)) continue;
+
+      const full_path = path.join(input, file);
+      const full_import_path = path.join(
+        value,
+        import_path.slice(alias.length)
+      );
+      let resolved = posixify(
+        path.relative(path.dirname(full_path), full_import_path)
+      );
+      resolved = resolved.startsWith(".") ? resolved : "./" + resolved;
+      return match.replace(import_path, resolved);
+    }
+    return match;
+  };
+
+  content = content.replace(/from\s+('|")([^"';,]+?)\1/g, replace_import_path);
+  content = content.replace(
+    /import\s*\(\s*('|")([^"';,]+?)\1\s*\)/g,
+    replace_import_path
+  );
+  return content;
+}
+
+/**
+ * Strip out lang="X" or type="text/X" tags. Doing it here is only a temporary solution.
+ * See https://github.com/sveltejs/kit/issues/2450 for ideas for places where it's handled better.
+ *
+ * @param {string} content
+ */
+function strip_lang_tags(content) {
+  return content
+    .replace(
+      /(<!--[^]*?-->)|(<script[^>]*?)\s(?:type|lang)=(["'])(.*?)\3/g,
+      // things like application/ld+json should be kept as-is. Preprocessed languages are "ts" etc
+      (match, s1, s2, _, s4) =>
+        s4?.startsWith("application/") ? match : (s1 ?? "") + (s2 ?? "")
+    )
+    .replace(
+      /(<!--[^]*?-->)|(<style[^>]*?)\s(?:type|lang)=(["']).*?\3/g,
+      "$1$2"
+    );
+}
+
+/**
+ * @param {string} file
+ * @param {Parameters<typeof fs.writeFileSync>[1]} contents
+ */
+function write(file, contents) {
+  mkdirp(path.dirname(file));
+  fs.writeFileSync(file, contents);
+}
+
+/**
+ * @param {string} input
+ * @param {string[]} extensions
+ * @returns {import('./types').File[]}
+ */
+function scan(input, extensions) {
+  return walk(input).map((file) => analyze(file, extensions));
+}
+
+/**
+ * @param {string} file
+ * @param {string[]} extensions
+ * @returns {import('./types').File}
+ */
+function analyze(file, extensions) {
+  const name = posixify(file);
+
+  const svelte_extension = extensions.find((ext) => name.endsWith(ext));
+
+  const base = svelte_extension
+    ? name
+    : name.slice(0, -path.extname(name).length);
+
+  const dest = svelte_extension
+    ? name.slice(0, -svelte_extension.length) + ".svelte"
+    : name.endsWith(".d.ts")
+    ? name
+    : name.endsWith(".ts")
+    ? name.slice(0, -3) + ".js"
+    : name;
+
+  return {
+    name,
+    dest,
+    base,
+    is_svelte: !!svelte_extension,
+  };
+}
