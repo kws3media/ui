79e6a8e022bb7b09020159d14ea05aab8e7545a3
nikeetaa231 <nikeetaa231@gmail.com>
2023-04-26 19:38:39 +0530

    helper import path changes

--- scripts/svelte2tsx/generator.js
+++ scripts/svelte2tsx/generator.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 var fs = require("fs");
 var { resolve, join } = require("path");
+const glob = require("glob");
 const path = require("path");
 // var createRequire = require("module");
-var { posixify, mkdirp, rimraf, walk } = require("./helpers/filesystem.js");
-var { resolve_aliases, write } = require("./helpers/utils.js");
+var { posixify, mkdirp, rimraf, walk } = require("../utils/filesystem.js");
+var { resolve_aliases, write } = require("../utils/helper.js");
 const svelte2tsx = require("svelte2tsx");
 //var svelteShimsPath = require("svelte2tsx/svelte-shims.d.ts");
 
@@ -17,12 +18,12 @@ const input = resolve("packages/ui");
  * The files are written to a temporary location and those which should be kept
  * are sanitized ($lib alias resolved) and copied over to the destination folder.
  *
- * @param {string} input
- * @param {string} output
- * @param {Record<string, string>} alias
- * @param {import('./types').File[]} files
+//  * @param {string} input
+//  * @param {string} output
+//  * @param {Record<string, string>} alias
+//  * @param {import('./types').File[]} files
  */
-async function main(alias, files) {
+async function main() {
   const tmp = `./__package_types_tmp__`;
   rimraf(tmp);
   mkdirp(tmp);
@@ -36,24 +37,34 @@ async function main(alias, files) {
   const handwritten = new Set();
 
   // skip files that conflict with hand-written .d.ts
-  for (const file of files) {
-    if (file.name.endsWith(".d.ts")) {
-      handwritten.add(file.name);
+  glob("./packages/**/*.svelte", function (err, svelteFiles) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      if (svelteFiles.length) {
+        svelteFiles.forEach((file) => {
+          const filename = path.basename(file);
+          console.log(`Processing ${filename}`);
+          if (filename.endsWith(".d.ts")) {
+            handwritten.add(filename);
+          }
+        });
+      }
     }
-  }
+  });
 
   // resolve $lib alias (TODO others), copy into package dir
   for (const file of walk(tmp)) {
     const normalized = posixify(file);
 
-    if (handwritten.has(normalized)) {
-      console.warn(`Using $lib/${normalized} instead of generated .d.ts file`);
-    }
+    // if (handwritten.has(normalized)) {
+    //   console.warn(`Using $lib/${normalized} instead of generated .d.ts file`);
+    // }
 
     const source = fs.readFileSync(path.join(tmp, normalized), "utf8");
     write(
       path.join(output, normalized),
-      resolve_aliases(input, normalized, source, alias)
+      resolve_aliases(input, normalized, source, alia)
     );
   }
 
