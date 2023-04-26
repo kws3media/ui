0c7319819067bcb9f3cf9d0ef82a86fab0e141ba
Sharif Ahmed <me.sharifahmed@gmail.com>
2023-04-26 19:45:59 +0600

    removed kit imports preprocess package

--- packages/@kws3/sveltekit-preprocess/src/index.js
+++
@@ -1,9 +0,0 @@
-import resolveComponentsPath from "./preprocessors/resolveComponentsPath";
-
-export function optimizeImports() {
-  return {
-    script: ({ content }) => {
-      return { code: resolveComponentsPath(content) };
-    },
-  };
-}
