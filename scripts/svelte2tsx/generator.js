20e6ff2f5a2701f65db66d59f7113567d93d87d3
nikeetaa231 <nikeetaa231@gmail.com>
2023-04-27 15:50:04 +0530

    WIP svelte2tsx generator

--- scripts/svelte2tsx/generator.js
+++ scripts/svelte2tsx/generator.js
@@ -4,14 +4,24 @@ var { resolve, join } = require("path");
 const glob = require("glob");
 const path = require("path");
 // var createRequire = require("module");
-var { posixify, mkdirp, rimraf, walk } = require("../utils/filesystem.js");
-var { resolve_aliases, write } = require("../utils/helper.js");
+var { mkdirp, rimraf, walk } = require("../utils/filesystem.js");
+var { write } = require("../utils/helper.js");
 const svelte2tsx = require("svelte2tsx");
 //var svelteShimsPath = require("svelte2tsx/svelte-shims.d.ts");
 
 const cwd = join(__dirname);
 const output = join(cwd, "dist");
 const input = resolve("packages/ui");
+
+glob("./packages/**/*.svelte", function (err, svelteFiles) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    if (svelteFiles.length) {
+      main(svelteFiles);
+    }
+  }
+});
 
 /**
  * Generates d.ts files by invoking TypeScript's "emit d.ts files from input files".
@@ -23,52 +33,32 @@ const input = resolve("packages/ui");
 //  * @param {Record<string, string>} alias
 //  * @param {import('./types').File[]} files
  */
-async function main() {
-  const tmp = `./__package_types_tmp__`;
+async function main(files) {
+  console.log("Input:" + input + "\n");
+  console.log("output:" + output + "\n");
+  console.log("cwd:" + cwd + "\n");
+  console.log("files:" + files.length + "\n");
+
+  const tmp = `${output}/`;
   rimraf(tmp);
   mkdirp(tmp);
 
   await svelte2tsx.emitDts({
     libRoot: input,
     svelteShimsPath: require.resolve("svelte2tsx/svelte-shims.d.ts"),
-    declarationDir: path.relative(__dirname, tmp),
-  });
-
-  const handwritten = new Set();
-
-  // skip files that conflict with hand-written .d.ts
-  glob("./packages/**/*.svelte", function (err, svelteFiles) {
-    if (err) {
-      console.log("Error", err);
-    } else {
-      if (svelteFiles.length) {
-        svelteFiles.forEach((file) => {
-          const filename = path.basename(file);
-          console.log(`Processing ${filename}`);
-          if (filename.endsWith(".d.ts")) {
-            handwritten.add(filename);
-          }
-        });
-      }
-    }
+    declarationDir: path.relative(cwd, tmp),
   });
 
   // resolve $lib alias (TODO others), copy into package dir
   for (const file of walk(tmp)) {
-    const normalized = posixify(file);
+    //const normalized = posixify(file);
+    //console.log(`Normalizing: ${file}`);
 
-    // if (handwritten.has(normalized)) {
-    //   console.warn(`Using $lib/${normalized} instead of generated .d.ts file`);
-    // }
-
-    const source = fs.readFileSync(path.join(tmp, normalized), "utf8");
-    write(
-      path.join(output, normalized),
-      resolve_aliases(input, normalized, source, alia)
-    );
+    const source = fs.readFileSync(tmp, "utf8");
+    write(output, source);
   }
 
-  rimraf(tmp);
+  //rimraf(tmp);
 }
 
 main().catch((err) => console.log(err.message));
