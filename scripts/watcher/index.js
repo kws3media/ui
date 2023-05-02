c2f4ade2858da044bd5b20057278583980c89a1c
sunnypol92 <suneeshsk3@gmail.com>
2023-05-02 21:59:33 +0530

    use simple methods

--- scripts/watcher/index.js
+++ scripts/watcher/index.js
@@ -6,6 +6,7 @@ const { existsSync, rmSync } = require("fs");
 const scriptDir = "./scripts/dts_generator";
 const watchDir = "./packages/@kws3/ui";
 
+//generate d.ts, d.ts.map files
 const generateDts = (filepath) => {
   const { stdout, stderr } = spawn("node", [`${scriptDir}/index.js`, filepath]);
 
@@ -13,31 +14,30 @@ const generateDts = (filepath) => {
   stderr.on("data", (data) => console.error(`stderr: ${data}`));
 };
 
+//remove d.ts, d.ts.map files
+const removeDts = (filepath) => {
+  console.log(`File ${filepath} has been removed`);
+
+  const fileName = `${path.dirname(filepath)}/${path.basename(
+    filepath,
+    path.extname(filepath)
+  )}`;
+
+  [`${fileName}.d.ts`, `${fileName}.d.ts.map`].forEach((file) => {
+    if (existsSync(file)) {
+      rmSync(file);
+      watcher.unwatch(file);
+      console.log(`File ${file} has been removed`);
+    }
+  });
+};
+
+//watch files
 const watcher = chokidar.watch(watchDir, { persistent: true });
 
-//missing `addDir`, `unlinkDir` events
 watcher
-  .on("add", (filepath) => generateDts(filepath))
-  .on("change", (filepath) => generateDts(filepath))
-  .on("unlink", (filepath) => {
-    console.log(`File ${filepath} has been removed`);
-
-    try {
-      const fileName = `${path.dirname(filepath)}/${path.basename(
-        filepath,
-        path.extname(filepath)
-      )}`;
-
-      [`${fileName}.d.ts`, `${fileName}.d.ts.map`].forEach((file) => {
-        if (existsSync(file)) {
-          rmSync(file);
-          watcher.unwatch(file);
-          console.log(`File ${file} has been removed`);
-        }
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  })
+  .on("add", generateDts)
+  .on("change", generateDts)
+  .on("unlink", removeDts)
   .on("ready", () => console.log("Initial scan complete. Ready for changes"))
   .on("error", (error) => console.log(`Watcher error: ${error}`));
