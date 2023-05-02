a6c83fb68b86fcb712c6419c20bbe9fcd23cc7cc
sunnypol92 <suneeshsk3@gmail.com>
2023-05-02 21:42:28 +0530

    unlink file watch update

--- scripts/watcher/index.js
+++ scripts/watcher/index.js
@@ -1,5 +1,7 @@
 const chokidar = require("chokidar");
 const { spawn } = require("child_process");
+const path = require("path");
+const { existsSync, rmSync } = require("fs");
 
 const scriptDir = "./scripts/dts_generator";
 const watchDir = "./packages/@kws3/ui";
@@ -17,6 +19,25 @@ const watcher = chokidar.watch(watchDir, { persistent: true });
 watcher
   .on("add", (filepath) => generateDts(filepath))
   .on("change", (filepath) => generateDts(filepath))
-  .on("unlink", (filepath) => generateDts(filepath))
+  .on("unlink", (filepath) => {
+    console.log(`File ${filepath} has been removed`);
+
+    try {
+      const fileName = `${path.dirname(filepath)}/${path.basename(
+        filepath,
+        path.extname(filepath)
+      )}`;
+
+      [`${fileName}.d.ts`, `${fileName}.d.ts.map`].forEach((file) => {
+        if (existsSync(file)) {
+          rmSync(file);
+          watcher.unwatch(file);
+          console.log(`File ${file} has been removed`);
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  })
   .on("ready", () => console.log("Initial scan complete. Ready for changes"))
   .on("error", (error) => console.log(`Watcher error: ${error}`));
