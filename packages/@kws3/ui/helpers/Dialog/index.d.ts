0952a3bb6801c45979c70e10d6368c478a783770
ekhaled <me.khaled@gmail.com>
2023-05-03 16:15:14 +0100

    dialog method types

--- packages/@kws3/ui/helpers/Dialog/index.d.ts
+++ packages/@kws3/ui/helpers/Dialog/index.d.ts
@@ -1,7 +1,22 @@
 /// <reference types="svelte/types/runtime/ambient" />
-export function alert(msg: any, props: any): Promise<any>;
-export function prompt(msg: any, props: any): Promise<any>;
-export function confirm(msg: any, props: any): Promise<any>;
+/**
+ *
+ * @param {string} msg
+ * @param {object?} props
+ */
+export function alert(msg: string, props: object | null): Promise<any>;
+/**
+ *
+ * @param {string} msg
+ * @param {object?} props
+ */
+export function prompt(msg: string, props: object | null): Promise<any>;
+/**
+ *
+ * @param {string} msg
+ * @param {object?} props
+ */
+export function confirm(msg: string, props: object | null): Promise<any>;
 export default Dialog;
 import Dialog from "./Dialog.svelte";
 //# sourceMappingURL=index.d.ts.map
