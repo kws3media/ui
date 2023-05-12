14cc44f95bf74e8a5d8aefc76a18361311e82754
Nurul Amin <namin2750@gmail.com>
2023-05-12 19:48:06 +0600

    fix

--- packages/@kws3/ui/forms/PasswordInput.svelte
+++ packages/@kws3/ui/forms/PasswordInput.svelte
@@ -3,16 +3,23 @@
 
 
   @param {boolean} [show_field_icon=true] - wheter to show field icon or not, Default: `true`
-  @param {string} [field_icon="lock"] - field icon name, Default: `lock`
-  @param {FontFamilies} [field_icon_family="fa"] - Icon family to be used, Default: `fa`
+  @param {string} [field_icon="lock"] - field icon name, Default: `"lock"`
+  @param {FontFamilies} [field_icon_family=fa] - Icon family to be used
 
+Defaults to global family set via `Icon.setDefaultIconType()`
+
+Ultimately defaults to `fa`, if family is not set anywhere, Default: `fa`
   @param {boolean} [show_visibility_switch=true] - whether has visibility feature or not, Default: `true`
-  @param {FontFamilies} [visibility_switch_icon_family="fa"] - Icon family to be used, Default: `fa`
-  @param {string} [visibility_switch_on_icon="eye"] - visibility switch on icon name, Default: `eye`
-  @param {string} [visibility_switch_off_icon="eye-slash"] - visibility switch off icon name, Default: `eye-slash`
-  @param {string} [visibility_switch_on_color="grey"] - visibility switch on color, Default: `grey`
-  @param {string} [visibility_switch_off_color="grey"] - visibility switch off color, Default: `grey`
+  @param {FontFamilies} [visibility_switch_icon_family=fa] - Icon family to be used
 
+Defaults to global family set via `Icon.setDefaultIconType()`
+
+Ultimately defaults to `fa`, if family is not set anywhere, Default: `fa`
+  @param {string} [visibility_switch_on_icon="eye"] - visibility switch on icon name, Default: `"eye"`
+  @param {string} [visibility_switch_off_icon="eye-slash"] - visibility switch off icon icon name, Default: `"eye-slash"`
+  @param {ExtendedColorOptions} [visibility_switch_on_color=grey] - visibility switch on icon color, Default: `grey`
+  @param {ExtendedColorOptions} [visibility_switch_off_color=grey] - visibility switch off icon color, Default: `grey`
+  @param {?string} [value=] - Value of the Input
 
 This property can be bound to, to fetch the current value, Default: ``
   @param {SizeOptions} [size=] - Size of the PasswordInput, Default: ``
@@ -20,31 +27,33 @@ This property can be bound to, to fetch the current value, Default: ``
   @param {string} [style=""] - Inline CSS for the PasswordInput, Default: `""`
   @param {boolean} [disabled=false] - Disables the PasswordInput, Default: `false`
   @param {string} [placeholder=""] - Input placeholder, Default: `""`
-  @param {FontFamilies} [icon_family=] - Icon family to be used
-
-Defaults to global family set via `Icon.setDefaultIconType()`
-
-Ultimately defaults to `fa`, if family is not set anywhere, Default: ``
   @param {string} [class=""] - CSS classes of the PasswordInput, Default: `""`
+
+  ### Events
+  - `input`
 
 -->
 
 <div class="field is-marginless">
   <p class="control has-icons-left has-icons-right is-marginless">
     <input
-      {type}
+      type="password"
+      bind:this={PASSWORD_INPUT}
       {style}
-      {value}
+      bind:value
       class="input is-{size} is-{color} {klass} {style}"
-      on:input={(event) => onInput(event)}
+      on:input
       {placeholder}
       {disabled} />
     {#if show_field_icon}
       <span class="icon is-left">
-        <Icon family={field_icon_family} icon={field_icon} size={iconSizes[size]} />
+        <Icon
+          family={field_icon_family}
+          icon={field_icon}
+          size={iconSizes[size]} />
       </span>
     {/if}
-   
+
     {#if show_visibility_switch}
       <span
         class="icon is-right visibility-btn"
@@ -53,8 +62,12 @@ Ultimately defaults to `fa`, if family is not set anywhere, Default: ``
         }}>
         <Icon
           family={visibility_switch_icon_family}
-          color={visibility ? visibility_switch_on_color : visibility_switch_off_color}
-          icon={visibility ? visibility_switch_on_icon : visibility_switch_off_icon}
+          color={visibility
+            ? visibility_switch_on_color
+            : visibility_switch_off_color}
+          icon={visibility
+            ? visibility_switch_on_icon
+            : visibility_switch_off_icon}
           size={iconSizes[size]} />
       </span>
     {/if}
@@ -73,13 +86,15 @@ Ultimately defaults to `fa`, if family is not set anywhere, Default: ``
   /**
    *
    * @typedef {import('@kws3/ui/types').ColorOptions} ColorOptions
+   * @typedef {import('@kws3/ui/types').ExtendedColorOptions} ExtendedColorOptions
    * @typedef {import('@kws3/ui/types').SizeOptions} SizeOptions
    * @typedef {import('@kws3/ui/types').FontFamilies} FontFamilies
    *
-   */ 
+   */
   let visibility = false;
-  
-   /**
+  let PASSWORD_INPUT;
+
+  /**
    * wheter to show field icon or not
    * @type {boolean}
    */
@@ -107,7 +122,7 @@ Ultimately defaults to `fa`, if family is not set anywhere, Default: ``
    * @type {boolean}
    */
   export let show_visibility_switch = true;
-    /**
+  /**
    * Icon family to be used
    *
    * Defaults to global family set via `Icon.setDefaultIconType()`
@@ -121,20 +136,20 @@ Ultimately defaults to `fa`, if family is not set anywhere, Default: ``
    * visibility switch on icon name
    * @type {string}
    */
-  export let visibility_switch_on_icon= 'eye';
+  export let visibility_switch_on_icon = "eye";
   /**
    * visibility switch off icon icon name
    * @type {string}
    */
-  export let visibility_switch_off_icon= 'eye-slash';
+  export let visibility_switch_off_icon = "eye-slash";
   /**
    * visibility switch on icon color
-   * @type {string}
+   * @type {ExtendedColorOptions}
    */
   export let visibility_switch_on_color = "grey";
   /**
    * visibility switch off icon color
-   * @type {string}
+   * @type {ExtendedColorOptions}
    */
   export let visibility_switch_off_color = "grey";
 
@@ -171,7 +186,7 @@ Ultimately defaults to `fa`, if family is not set anywhere, Default: ``
    * @type {string}
    */
   export let placeholder = "";
- 
+
   /**
    * CSS classes of the PasswordInput
    * @type {string}
@@ -179,18 +194,25 @@ Ultimately defaults to `fa`, if family is not set anywhere, Default: ``
   let klass = "";
   export { klass as class };
 
-  function onInput(event) {
-    value = event.target.value;
-  }
+  // function onInput(event) {
+  //   value = event.target.value;
+  // }
 
   let iconSizes = {
     large: "medium",
   };
 
-  $: type = visibility ? "text" : "password";
+  $: visibility, setInputType();
+
+  function setInputType() {
+    if (PASSWORD_INPUT) {
+      PASSWORD_INPUT.type = visibility ? "text" : "password";
+    }
+  }
 
   // $: visibility_icon = {
   //   show: visibility_switch_icon_family === "material" ? "visibility" : visibility_switch_on_icon,
   //   hide: visibility_switch_icon_family === "material" ? "visibility-off" : visibility_switch_off_icon,
   // };
+  // on:input={(event) => onInput(event)}
 </script>
