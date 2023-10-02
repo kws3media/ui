698c212d9f14c20d7d9a98e0ab0e395fac320892
Sabir SM <sabirveli@gmail.com>
2023-10-02 16:35:57 +0530

    Button stories WIP

---
+++ src/stories_new/buttons/DeleteButton/deleteButton.play.js
@@ -0,0 +1,58 @@
+import { expect } from "@storybook/jest";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+
+// eslint-disable-next-line no-unused-vars
+export default async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  let buttons = canvas.getAllByRole("button");
+  const successButton = buttons[0];
+  const failButton = buttons[1];
+
+  //click on the success button
+  //it will show cancel button
+  await userEvent.click(successButton);
+  buttons = canvas.getAllByRole("button");
+  const cancelButton = buttons[0];
+  await expect(Number(buttons.length)).toEqual(3);
+
+  //click on the cancel button
+  //it will hide cancel button
+  await userEvent.click(cancelButton);
+  buttons = canvas.getAllByRole("button");
+  await expect(Number(buttons.length)).toEqual(2);
+
+  //again click on the success button
+  //it will confirm the action
+  //and show the processed result
+  await userEvent.dblClick(successButton);
+  await waitFor(
+    () => {
+      expect(successButton.innerText).toBe("Deleted");
+    },
+    { timeout: 2000 }
+  );
+  await waitFor(
+    () => {
+      expect(successButton.innerText).toBe("Delete Me");
+    },
+    { timeout: 600 }
+  );
+
+  //click on the fail button
+  //it will confirm the action
+  //and show the processed result
+  await userEvent.dblClick(failButton);
+  await waitFor(
+    () => {
+      expect(failButton.innerText).toBe("Failed");
+    },
+    { timeout: 2000 }
+  );
+  await waitFor(
+    () => {
+      expect(failButton.innerText).toBe("Delete Me");
+    },
+    { timeout: 3000 }
+  );
+};
---
+++ src/stories_new/buttons/ProcessButton/processButton.play.js
@@ -0,0 +1,43 @@
+import { expect } from "@storybook/jest";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+
+// eslint-disable-next-line no-unused-vars
+export default async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  let buttons = canvas.getAllByRole("button");
+  const successButton = buttons[0];
+  const failButton = buttons[1];
+
+  //click on the success button
+  //and show the processed result
+  await userEvent.click(successButton);
+  await waitFor(
+    () => {
+      expect(successButton.innerText).toBe("Done");
+    },
+    { timeout: 2000 }
+  );
+  await waitFor(
+    () => {
+      expect(successButton.innerText).toBe("Click Me");
+    },
+    { timeout: 600 }
+  );
+
+  //click on the fail button
+  //and show the processed result
+  await userEvent.click(failButton);
+  await waitFor(
+    () => {
+      expect(failButton.innerText).toBe("Failed");
+    },
+    { timeout: 2000 }
+  );
+  await waitFor(
+    () => {
+      expect(failButton.innerText).toBe("Click Me");
+    },
+    { timeout: 3000 }
+  );
+};
