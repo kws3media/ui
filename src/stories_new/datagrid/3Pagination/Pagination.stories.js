71177631c8e98d6294be8d7b66edb201f0ceafb7
Sabir SM <sabirveli@gmail.com>
2023-10-12 17:31:02 +0530

    fix

--- src/stories_new/datagrid/3Pagination/Pagination.stories.js
+++ src/stories_new/datagrid/3Pagination/Pagination.stories.js
@@ -1,27 +1,31 @@
 import Decorator from "./Pagination.svelte";
-import { Pagination } from '@kws3/ui';
+import { Pagination } from "@kws3/ui";
 
 let args = {
-  meta : {},
+  meta: {},
   limit: 20,
   total: 120,
   count: 20,
   offset: 20,
-  showTotal : true,
-  showCurrent : true,
-  showPerPage : true,
-  maxVisiblePages : 10,
-  entityName : "entries",
-  size : "small",
-  frame : false,
-  iconRight : "chevron-right",
-  iconLeft : "chevron-left",
-  perPageOptions : [20, 50, 100, 150, 200, 250]
+  showTotal: true,
+  showCurrent: true,
+  showPerPage: true,
+  maxVisiblePages: 10,
+  entityName: "entries",
+  size: "small",
+  frame: false,
+  iconRight: "chevron-right",
+  iconLeft: "chevron-left",
+  perPageOptions: [20, 50, 100, 150, 200, 250],
 };
 
 export default {
   title: "Datagrid/Pagination",
   component: Pagination,
+  parameters: {
+    layout: "fullscreen",
+    canvas: { hidden: true },
+  },
   args: args,
   argTypes: {},
 };
