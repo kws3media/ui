c90adf19a8dc24719ff0d963727be7332e9bcc39
Sabir SM <sabirveli@gmail.com>
2023-10-16 19:42:20 +0530

    Radio story

---
+++ src/stories_new/Controls/Radio/Radio.stories.js
@@ -0,0 +1,37 @@
+import { Radio } from "@kws3/ui";
+
+let args = {
+  value: null,
+  class: "",
+  style: "",
+  label_style: "",
+  size: "",
+  color: "",
+  inverted: false,
+  circle: false,
+  disabled: false,
+  group: null,
+};
+
+let argTypes = {
+  color: {
+    control: "select",
+  },
+  size: {
+    control: "select",
+  },
+};
+
+export default {
+  title: "Forms/Controls/Radio",
+  component: Radio,
+  args: args,
+  argTypes: argTypes,
+};
+
+export const Main = {
+  name: "Preview",
+  args: args,
+  argTypes: argTypes,
+  props: args,
+};
