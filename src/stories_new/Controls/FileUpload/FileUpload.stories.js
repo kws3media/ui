e8183b9b15c29c89a8584aa389bb7355b578ee1f
Sabir SM <sabirveli@gmail.com>
2023-10-16 19:40:54 +0530

    File upload story

---
+++ src/stories_new/Controls/FileUpload/FileUpload.stories.js
@@ -0,0 +1,41 @@
+import Decorator from "./FileUpload.svelte";
+import { FileUpload } from "@kws3/ui";
+
+let args = {
+  info: "",
+  key: "",
+  message: "Choose File...",
+  max: 5000000,
+  allowed: "*",
+  color: "",
+  info_color: "grey",
+  class: "",
+  disabled: false,
+};
+
+let argTypes = {
+  color: {
+    control: "select",
+  },
+  size: {
+    control: "select",
+  },
+  info_color: {
+    control: "select",
+  },
+};
+
+export default {
+  title: "Forms/Controls/FileUpload",
+  component: FileUpload,
+  decorators: [() => Decorator],
+  args: args,
+  argTypes: argTypes,
+};
+
+export const Main = {
+  name: "Preview",
+  args: args,
+  argTypes: argTypes,
+  props: args,
+};
