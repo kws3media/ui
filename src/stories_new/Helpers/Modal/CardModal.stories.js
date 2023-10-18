879989d901848b6f763aee78a62b25f93b483a1e
Sabir SM <sabirveli@gmail.com>
2023-10-18 21:23:22 +0530

    Modal stories

---
+++ src/stories_new/Helpers/Modal/CardModal.stories.js
@@ -0,0 +1,33 @@
+import { CardModal } from "@kws3/ui";
+import CardModalDecorator from "./CardModal.svelte";
+
+let args = {
+  title: "Card Modal",
+  size: "small",
+  is_active: false,
+  closable: true,
+  close_on_click_outside: false,
+  has_footer: true,
+  class: "",
+  style: "",
+  inner_class: "",
+  inner_style: "",
+  cy: "",
+};
+
+let argTypes = {};
+
+export default {
+  title: "Helpers/Modal/CardModal",
+  component: CardModal,
+  decorators: [() => CardModalDecorator],
+  args: args,
+  argTypes: argTypes,
+};
+
+export const Main = {
+  name: "Preview",
+  args: args,
+  argTypes: argTypes,
+  props: args,
+};
---
+++ src/stories_new/Helpers/Modal/Modal.stories.js
@@ -0,0 +1,38 @@
+import { Modal } from "@kws3/ui";
+import ModalDecorator from "./Modal.svelte";
+
+let args = {
+  size: "small",
+  class: "",
+  style: "",
+  inner_class: "",
+  inner_style: "",
+  is_active: false,
+  closable: true,
+  close_on_click_outside: false,
+  cy: "",
+};
+
+let argTypes = {
+  color: {
+    control: "select",
+  },
+  size: {
+    control: "select",
+  },
+};
+
+export default {
+  title: "Helpers/Modal/Modal",
+  component: Modal,
+  decorators: [() => ModalDecorator],
+  args: args,
+  argTypes: argTypes,
+};
+
+export const Main = {
+  name: "Preview",
+  args: args,
+  argTypes: argTypes,
+  props: args,
+};
