ea5525be23efe212c16e687bb33437f3d4b6fbde
Sabir SM <sabirveli@gmail.com>
2023-10-19 19:33:57 +0530

    Skeleton stories

---
+++ src/stories_new/Helpers/Skeleton/Skeleton.stories.js
@@ -0,0 +1,27 @@
+import { Skeleton } from "@kws3/ui";
+
+let args = {
+  class: "",
+  lines: 1,
+  width: "100%",
+  height: "auto",
+  radius: "0px",
+  circle: false,
+  color: "primary",
+};
+
+let argTypes = {};
+
+export default {
+  title: "Helpers/Skeleton",
+  component: Skeleton,
+  args: args,
+  argTypes: argTypes,
+};
+
+export const Main = {
+  name: "Preview",
+  args: args,
+  argTypes: argTypes,
+  props: args,
+};
