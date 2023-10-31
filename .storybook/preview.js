3eeb58f602b9cf18f5fd30b53be70e16201c3611
Sabir SM <sabirveli@gmail.com>
2023-10-31 21:54:39 +0530

    altering preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,28 +1,16 @@
 import "./../src/scss/app.scss";
-export const parameters = {
-  layout: "centered",
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    hideNoControlsWarning: true,
-    expanded: false,
-  },
-  docs: { page: null },
-  options: { showPanel: true },
-  backgrounds: { disable: true },
-  viewMode: "docs",
-};
 
-/*
-themes: {
-    //clearable: false,
-    default: "Light",
-    list: [
-      { name: "Light", class: "theme-kws-light", color: "#F6F6F6" },
-      { name: "Dark", class: "theme-kws-dark", color: "#333333" },
-    ],
-    onChange: (theme) => {
-      console.log("THEME:", theme);
-      // todo : if needed
+export default {
+  parameters: {
+    layout: "centered",
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      hideNoControlsWarning: true,
+      expanded: false,
     },
+    docs: { page: null },
+    options: { showPanel: true },
+    backgrounds: { disable: true },
+    viewMode: "docs",
   },
-*/
+};
