531aae5b16a11d9cfe161184b5e82f937787289b
Sabir SM <sabirveli@gmail.com>
2023-11-09 18:08:49 +0530

    some fixes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+//import { mergeConfig } from "vite";
+import path from "path";
+import sveltePreprocess from "svelte-preprocess";
+
 export default {
   stories: [
     "../src/stories_new/_index/index.stories.mdx",
@@ -16,6 +20,7 @@ export default {
   },
   addons: [
     "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf",
     "@storybook/addon-actions",
     "@storybook/addon-interactions",
     "@storybook/addon-viewport",
@@ -23,11 +28,27 @@ export default {
     "@storybook/preset-svelte",
     "@storybook/addon-controls",
   ],
+  svelteOptions: {
+    preprocess: sveltePreprocess(),
+  },
+  core: {
+    builder: "@storybook/builder-vite",
+  },
   framework: {
     name: "@storybook/svelte-vite",
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
   },
+  // async viteFinal(config, { configType }) {
+  //   config.plugins
+  //     .push
+  //     // Example Vite plugin usage
+  //     // yourVitePlugin({ option: 'value' })
+  //     ();
+  //   // Example modification of Vite config
+  //   config.resolve.alias["~"] = path.resolve(__dirname, "../src");
+  //   return config;
+  // },
 };
