cf906705390fe1e0b7f04bc472480b521a3fbe76
Sabir SM <sabirveli@gmail.com>
2023-11-14 19:18:16 +0530

    undo vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
-import sveltePreprocess from "svelte-preprocess";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,12 +10,28 @@ export default defineConfig({
       "~": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [
-    svelte({
-      preprocess: [sveltePreprocess({ typescript: true })],
-    }),
-  ],
-  optimizeDeps: {
-    include: ["svelte", path.resolve(__dirname, "./packages/@kws3/ui")],
-  },
+  plugins: [svelte()],
 });
+
+// import { defineConfig } from "vite";
+// import { svelte } from "@sveltejs/vite-plugin-svelte";
+// import path from "path";
+// import sveltePreprocess from "svelte-preprocess";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   resolve: {
+//     alias: {
+//       "@kws3": path.resolve(__dirname, "./packages/@kws3"),
+//       "~": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   plugins: [
+//     svelte({
+//       preprocess: [sveltePreprocess({ typescript: true })],
+//     }),
+//   ],
+//   optimizeDeps: {
+//     include: ["svelte", path.resolve(__dirname, "./packages/@kws3/ui")],
+//   },
+// });
