b95479918129c3df9e9cff2dca5a15e0ced19a6c
Sharif Ahmed <me.sharifahmed@gmail.com>
2023-11-20 22:14:51 +0600

    fix

--- src/stories/forms/Select/SearchableSelect/SearchableSelect.svelte
+++ src/stories/forms/Select/SearchableSelect/SearchableSelect.svelte
@@ -92,7 +92,7 @@
     placeholder = "Choose a Brand...",
     selected_brand = "", // single , also work with { id: 10, name: "Blackberry" }
     //selected_brand_multi = "", // max item null, not works if string given. should work?
-    selected_brand_string = "BlackBerry";
+    selected_brand_string = "Blackberry";
 
   let klass = "";
   export { klass as class };
