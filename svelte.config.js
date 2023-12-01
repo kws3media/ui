f27d707ee3b02f7e7bca1f931ce105c772b432cf
Sharif Ahmed <me.sharifahmed@gmail.com>
2023-12-01 21:17:30 +0600

    suppress most a11y warnings

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,11 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  onwarn: (warning, handler) => {
+    if (warning.code.startsWith("a11y-")) return;
+    handler(warning);
+  },
+  vitePlugin: {
+    inspector: true,
+  },
 };
