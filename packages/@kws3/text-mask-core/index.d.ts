51b481ac851fa00cd850866fbf6d1dec28482427
ekhaled <me.khaled@gmail.com>
2023-12-22 17:40:39 +0000

    remove unwanted files

--- packages/@kws3/text-mask-core/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as conformToMask } from "./utils/conformToMask";
-export { default as adjustCaretPosition } from "./utils/adjustCaretPosition";
-export { default as createTextMaskInputElement } from "./utils/createTextMaskInputElement";
-//# sourceMappingURL=index.d.ts.map
