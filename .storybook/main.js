edd7606969f83d61e6d0ffd08bb7ab47682f1df6
ekhaled <me.khaled@gmail.com>
2023-12-22 18:35:39 +0000

    Revert "try fixing order again"
    This reverts commit 6d81e383089f735600930680b6ab186594f2b387.

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,9 @@ import sveltePreprocess from "svelte-preprocess";
 
 export default {
   stories: [
-    "../src/stories_new/0.index/0index.mdx",
-    "../src/stories_new/0.index/1index*.*",
-    "../src/stories_new/0.index/2changelog.mdx",
+    "../src/stories_new/_index/index.mdx",
+    "../src/stories_new/_index/index*.*",
+    "../src/stories_new/_index/changelog.mdx",
     "../src/stories_new/index.stories.js",
     "../src/stories_new/**/*.mdx",
     "../src/stories_new/**/*.stories.*",
