eb230595f0db7d3842036d34d462da560654d754
ekhaled <me.khaled@gmail.com>
2024-02-08 18:10:33 +0000

    Use dynamic scroll shadows for `InfiniteList` component

--- packages/@kws3/ui/helpers/InfiniteList.svelte
+++ packages/@kws3/ui/helpers/InfiniteList.svelte
@@ -21,12 +21,14 @@ while more items are loading
 {#if hasResizeObserver}
   <div
     bind:this={viewport}
-    class="kws-scrollable-list kws-infinite-list with-resize-observer {klass}"
-    on:scroll={handle_scroll}
+    class="kws-infinite-list with-resize-observer {klass}"
     style="height:{height};{style}"
     use:resizeObserver
     on:resize={resize}>
-    <div bind:this={contents}>
+    <div
+      class="kws-infinite-list-inner"
+      on:scroll={handle_scroll}
+      bind:this={contents}>
       {#each visible as item (item.index)}
         <div class="row">
           <!--Default slot for list view items-->
@@ -34,18 +36,30 @@ while more items are loading
         </div>
       {/each}
       <!--Optional slot to display a loading graphic at the bottom of the list
-        while more items are loading-->
+          while more items are loading-->
       <slot name="loader" />
+    </div>
+    <div
+      class="fake-shadow-contraint top"
+      style="opacity:{needsScroll ? scale(scrollTop) : 0};">
+      <div class="fake-inner-shadow" />
+    </div>
+    <div
+      class="fake-shadow-contraint bottom"
+      style="opacity:{needsScroll ? 1 - scale(scrollTop) : 0};">
+      <div class="fake-inner-shadow" />
     </div>
   </div>
 {:else}
   <div
     bind:this={viewport}
-    class="kws-scrollable-list kws-infinite-list {klass}"
-    on:scroll={handle_scroll}
+    class="kws-infinite-list {klass}"
     style="height:{height};{style}"
-    bind:offsetHeight={viewport_height}>
-    <div bind:this={contents}>
+    bind:offsetHeight={viewportHeight}>
+    <div
+      class="kws-infinite-list-inner"
+      on:scroll={handle_scroll}
+      bind:this={contents}>
       {#each visible as item (item.index)}
         <div class="row">
           <!--Default slot for list view items-->
@@ -53,24 +67,72 @@ while more items are loading
         </div>
       {/each}
       <!--Optional slot to display a loading graphic at the bottom of the list
-        while more items are loading-->
+          while more items are loading-->
       <slot name="loader" />
+    </div>
+    <div
+      class="fake-shadow-contraint top"
+      style="opacity:{needsScroll ? scale(scrollTop) : 0};">
+      <div class="fake-inner-shadow" />
+    </div>
+    <div
+      class="fake-shadow-contraint bottom"
+      style="opacity:{needsScroll ? 1 - scale(scrollTop) : 0};">
+      <div class="fake-inner-shadow" />
     </div>
   </div>
 {/if}
 
 <style>
   .kws-infinite-list {
-    overflow: auto;
-    -webkit-overflow-scrolling: touch;
     position: relative;
     height: 100%;
+  }
+
+  .kws-infinite-list-inner {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  .fake-shadow-contraint {
+    position: absolute;
+    pointer-events: none;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 20px;
+    overflow: hidden;
+  }
+  .fake-shadow-contraint .fake-inner-shadow {
+    position: absolute;
+    left: 0;
+    top: -1px;
+    width: 100%;
+    height: 1px;
+    box-shadow:
+      0 0 6px 0 #000,
+      0 0 12px 0 #000,
+      0 0 18px 0 #000,
+      0 0 20px 0 #000;
+  }
+
+  .fake-shadow-contraint.bottom {
+    top: auto;
+    bottom: 0;
+  }
+
+  .fake-shadow-contraint.bottom .fake-inner-shadow {
+    top: auto;
+    bottom: -1px;
   }
 </style>
 
 <script>
   import { hasResizeObserver, resizeObserver } from "@kws3/ui/resizeObserver";
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, tick } from "svelte";
+  import { createScaler } from "@kws3/ui/utils";
 
   const fire = createEventDispatcher();
   /**
@@ -103,18 +165,28 @@ while more items are loading
   // local state
   let viewport,
     contents,
-    viewport_height = 0,
+    viewportHeight = 0,
+    scrollHeight = 0,
+    offsetHeight = 0,
     visible,
     fired = false;
+
+  let scale = createScaler([0, 1], [0, 1]);
+
+  let needsScroll = false,
+    scrollTop = 0;
 
   $: visible = items.slice().map((data, i) => ({
     index: iteration_key ? data[iteration_key] : i,
     data,
   }));
 
+  $: viewportHeight, visible, relayout();
+
   function handle_scroll() {
-    const { scrollTop, scrollHeight } = viewport;
-    let offset = scrollHeight - viewport_height - scrollTop;
+    scrollHeight = contents.scrollHeight;
+    scrollTop = contents.scrollTop;
+    let offset = scrollHeight - viewportHeight - scrollTop;
     // fire on:end event if we scrolled past the end_offset
     if (offset <= end_offset) {
       if (!fired) {
@@ -130,6 +202,21 @@ while more items are loading
   }
 
   const resize = () => {
-    viewport_height = viewport.offsetHeight;
+    viewportHeight = viewport.offsetHeight;
   };
+
+  export function relayout() {
+    tick().then(() => {
+      needsScroll = false;
+
+      scrollHeight = contents.scrollHeight;
+      offsetHeight = contents.offsetHeight;
+
+      let diff = scrollHeight - offsetHeight;
+      if (scrollHeight > offsetHeight) {
+        needsScroll = true;
+      }
+      scale = createScaler([0, diff], [0, 1]);
+    });
+  }
 </script>
