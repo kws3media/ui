8cc851fa53182ccd302aebf07d3379db1f25ecf6
ekhaled <me.khaled@gmail.com>
2024-02-08 19:28:20 +0000

    add hoverable for later ease of checking

--- src/stories_new/5.Datagrid/6StickyColumnsTable/StickyColumnsTable.svelte
+++ src/stories_new/5.Datagrid/6StickyColumnsTable/StickyColumnsTable.svelte
@@ -1,6 +1,6 @@
 <div style="width:900px;margin: 0 auto">
   <StickyColumnsTable {inner_class} {outer_class} {width}>
-    <table class="table kws-grid-view is-striped is-bordered">
+    <table class="table kws-grid-view is-striped is-bordered is-hoverable">
       <thead>
         <tr>
           <th class="is-sticky-column is-narrow" style="left: 0">Name</th>
