3cc2579e932d1248b4b7b3e21c9c558b7fb74408
ekhaled <me.khaled@gmail.com>
2024-04-03 17:24:41 +0100

    cleanup old stories from sb5/6

--- src/stories_new/4.Forms/Select/SearchableSelect/searchableselect.play.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-import { expect, within, userEvent, waitFor } from "@storybook/test";
-import { sleep } from "../../../../utils";
-
-export default async ({ args, canvasElement }) => {
-  const canvas = within(canvasElement);
-};
