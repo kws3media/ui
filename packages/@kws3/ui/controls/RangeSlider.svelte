e03cbc3aeb978d1d5cd0d802ee55d8b56c165203
ekhaled <me.khaled@gmail.com>
2024-07-01 13:16:24 +0100

    Rangeslider bugfix

--- packages/@kws3/ui/controls/RangeSlider.svelte
+++ packages/@kws3/ui/controls/RangeSlider.svelte
@@ -127,8 +127,8 @@ This will be overridden if `min` is higher, or `max` is lower, Default: `0`
   $: {
     let range = max - min;
     let position = ((value - min) / range) * 100;
-    let positionOffset = Math.round(position * 0.5);
-    computedStyle = `left:calc(${position}% - ${positionOffset}px`;
+    let positionOffset = Math.round(position * 0.4);
+    computedStyle = `left:calc(${position}% - ${positionOffset}px)`;
   }
 
   onMount(() => {
